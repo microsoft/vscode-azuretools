d916d5f1bcaeea9c6daa889a0131501160784c40
nturinski <naturins@microsoft.com>
2017-12-04 12:07:57 -0800

    Added whitespace

--- appservice/src/createAppService/StorageAccountStep.ts
+++ appservice/src/createAppService/StorageAccountStep.ts
@@ -59,7 +59,7 @@ export class StorageAccountStep extends WizardStep {
         newAccountName = await this.showInputBox({
             value: suggestedName,
                 prompt: 'Enter the name of the new storage account.',
-                validateInput: async (value: string): Promise <string | undefined>=> {
+                validateInput: async (value: string): Promise <string | undefined> => {
                     value = value ? value.trim() : '';
                     const nameAvailabilityResult: CheckNameAvailabilityResult = await storageClient.storageAccounts.checkNameAvailability(newAccountName);
                     if (!nameAvailabilityResult.nameAvailable) {
