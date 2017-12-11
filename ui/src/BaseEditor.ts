8f0ba542eaac2e9598b1bd0318a4a754c702df00
nturinski <naturins@microsoft.com>
2017-12-11 13:31:52 -0800

    TSlint changes

--- ui/src/BaseEditor.ts
+++ ui/src/BaseEditor.ts
@@ -6,60 +6,64 @@
 import * as fse from 'fs-extra';
 import * as path from 'path';
 import * as vscode from 'vscode';
-import { TemporaryFile } from './utils/temporaryFile';
 import { UserCancelledError } from './errors';
 import { localize } from "./localize";
+import { TemporaryFile } from './utils/temporaryFile';
 
-class DialogResponses {
-    static readonly OK: string = localize('OK', "OK");
-    static readonly DontShowAgain: string = localize('dontShow', "Don't Show Again");
-}
+// tslint:disable-next-line:typedef
+const dialogResponses = {
+    OK: localize('OK', "OK"),
+    DontShowAgain: localize('dontShow', "Don't Show Again")
+};
 
+// tslint:disable-next-line:no-unsafe-any
 export abstract class BaseEditor<ContextT> implements vscode.Disposable {
     private fileMap: { [key: string]: [vscode.TextDocument, ContextT] } = {};
     private ignoreSave: boolean = false;
 
-    abstract getData(context: ContextT): Promise<string>;
-    abstract updateData(context: ContextT, data: string): Promise<string>;
-    abstract getFilename(context: ContextT): Promise<string>;
-    abstract getSize(context: ContextT): Promise<number>;
-    abstract getSaveConfirmationText(context: ContextT): Promise<string>;
-
     constructor(readonly showSavePromptKey: string, readonly outputChanel?: vscode.OutputChannel) {
     }
 
+    public abstract getData(context: ContextT): Promise<string>;
+    public abstract updateData(context: ContextT, data: string): Promise<string>;
+    public abstract getFilename(context: ContextT): Promise<string>;
+    public abstract getSize(context: ContextT): Promise<number>;
+    public abstract getSaveConfirmationText(context: ContextT): Promise<string>;
+
     public async showEditor(context: ContextT): Promise<void> {
-        var size = await this.getSize(context);
-        var fileName = await this.getFilename(context);
-        var splitFileName = fileName.split(".");
-        var extension = splitFileName[splitFileName.length - 1];
+        const size: number = await this.getSize(context);
+        const fileName: string = await this.getFilename(context);
+        const splitFileName: string[] = fileName.split('.');
+        const extension: string = splitFileName[splitFileName.length - 1];
 
         this.appendToOutput(localize('opening', 'Opening "{0}" ...', fileName));
         if (size > 50 /*Megabytes*/) {
-            let message = localize('tooLargeError', '"{0}" is too large to download.', fileName);
+            const message: string = localize('tooLargeError', '"{0}" is too large to download.', fileName);
 
-            await vscode.window.showWarningMessage(message, DialogResponses.OK);
+            await vscode.window.showWarningMessage(message, dialogResponses.OK);
             this.appendLineToOutput(localize('failed', "Failed."));
             this.appendLineToOutput(localize('errorDetails', 'Error Details: {0}', message));
-        } else if (extension === "exe" || extension === "img" || extension === "zip") {
-            let message = localize('unsupportedError', '"{0}" has an unsupported file extension.', fileName);
+        } else if (extension === 'exe' || extension === 'img' || extension === 'zip') {
+            const message: string = localize('unsupportedError', '"{0}" has an unsupported file extension.', fileName);
 
-            await vscode.window.showWarningMessage(message, DialogResponses.OK);
+            await vscode.window.showWarningMessage(message, dialogResponses.OK);
             this.appendLineToOutput(localize('failed', " Failed."));
             this.appendLineToOutput(localize('errorDetails', 'Error Details: {0}', message));
         } else {
             try {
-                const localFilePath = await TemporaryFile.create(fileName)
-                const document = await vscode.workspace.openTextDocument(localFilePath);
+                const localFilePath: string = await TemporaryFile.create(fileName);
+                const document: vscode.TextDocument = await vscode.workspace.openTextDocument(localFilePath);
                 this.fileMap[localFilePath] = [document, context];
-                var data = await this.getData(context);
-                const textEditor = await vscode.window.showTextDocument(document);
+                const data: string = await this.getData(context);
+                const textEditor: vscode.TextEditor = await vscode.window.showTextDocument(document);
                 await this.updateEditor(data, textEditor);
-                this.appendLineToOutput(` Done.`);
+                this.appendLineToOutput(' Done.');
             } catch (error) {
-                var details: string;
+                let details: string;
 
+                // tslint:disable-next-line:no-unsafe-any
                 if (!!error.message) {
+                    // tslint:disable-next-line:no-unsafe-any
                     details = error.message;
                 } else {
                     details = JSON.stringify(error);
@@ -68,26 +72,63 @@ export abstract class BaseEditor<ContextT> implements vscode.Disposable {
                 this.appendLineToOutput(localize('failed', " Failed."));
                 this.appendLineToOutput(localize('errorDetails', 'Error Details: {0}', details));
 
-                await vscode.window.showWarningMessage(localize('downloadError', 'Unable to download "{0}". Please check Output for more information.', fileName), DialogResponses.OK);
+                await vscode.window.showWarningMessage(localize('downloadError', 'Unable to download "{0}". Please check Output for more information.', fileName), dialogResponses.OK);
             }
 
         }
     }
 
-    public async updateMatchingcontext(doc): Promise<void> {
-        const filePath = Object.keys(this.fileMap).find((filePath) => path.relative(doc.fsPath, filePath) === '');
+    public async updateMatchingcontext(doc: vscode.Uri): Promise<void> {
+        const filePath: string | undefined = Object.keys(this.fileMap).find((fsPath: string) => path.relative(doc.fsPath, fsPath) === '');
         if (filePath) {
-            var [textDocument, context] = this.fileMap[filePath];
+            const [textDocument, context]: [vscode.TextDocument, ContextT] = this.fileMap[filePath];
             await this.updateRemote(context, textDocument);
         }
     }
 
     public async dispose(): Promise<void> {
-        Object.keys(this.fileMap).forEach(async (key) => await fse.remove(path.dirname(key)));
+        // tslint:disable-next-line:no-unsafe-any
+        Object.keys(this.fileMap).forEach(async (key: string) => await fse.remove(path.dirname(key)));
+    }
+
+    public async onDidSaveTextDocument(globalState: vscode.Memento, doc: vscode.TextDocument): Promise<void> {
+        const filePath: string | undefined = Object.keys(this.fileMap).find((fsPath: string) => path.relative(doc.uri.fsPath, fsPath) === '');
+        if (!this.ignoreSave && filePath) {
+            const context: ContextT = this.fileMap[filePath][1];
+            const showSaveWarning: boolean | undefined = vscode.workspace.getConfiguration().get(this.showSavePromptKey);
+            if (showSaveWarning) {
+
+                const message: string = await this.getSaveConfirmationText(context);
+                const result: string | undefined = await vscode.window.showWarningMessage(message, dialogResponses.OK, dialogResponses.DontShowAgain);
+
+                if (!result) {
+                    throw new UserCancelledError();
+                } else if (result === dialogResponses.DontShowAgain) {
+                    await vscode.workspace.getConfiguration().update(this.showSavePromptKey, false, vscode.ConfigurationTarget.Global);
+                    await globalState.update(this.showSavePromptKey, true);
+                }
+            }
+
+            await this.updateRemote(context, doc);
+        }
+    }
+
+    protected appendToOutput(value: string): void {
+        if (!!this.outputChanel) {
+            this.outputChanel.append(value);
+            this.outputChanel.show(true);
+        }
+    }
+
+    protected appendLineToOutput(value: string): void {
+        if (!!this.outputChanel) {
+            this.outputChanel.appendLine(value);
+            this.outputChanel.show(true);
+        }
     }
 
     private async updateRemote(context: ContextT, doc: vscode.TextDocument): Promise<void> {
-        var filename = await this.getFilename(context);
+        const filename: string = await this.getFilename(context);
         this.appendToOutput(localize('updating', 'Updating "{0}" ...', filename));
         try {
             const updatedData: string = await this.updateData(context, doc.getText());
@@ -96,20 +137,6 @@ export abstract class BaseEditor<ContextT> implements vscode.Disposable {
         } catch (error) {
             this.appendLineToOutput(localize('failed', " Failed."));
             this.appendLineToOutput(localize('errorDetails', 'Error Details: {0}', error));
-        }
-    }
-
-    protected appendToOutput(value: string) {
-        if (!!this.outputChanel) {
-            this.outputChanel.append(value);
-            this.outputChanel.show(true);
-        }
-    }
-
-    protected appendLineToOutput(value: string) {
-        if (!!this.outputChanel) {
-            this.outputChanel.appendLine(value);
-            this.outputChanel.show(true);
         }
     }
 
@@ -124,33 +151,11 @@ export abstract class BaseEditor<ContextT> implements vscode.Disposable {
             }
         }
     }
-
-    public async onDidSaveTextDocument(globalState: vscode.Memento, doc: vscode.TextDocument): Promise<void> {
-        const filePath = Object.keys(this.fileMap).find((filePath) => path.relative(doc.uri.fsPath, filePath) === '');
-        if (!this.ignoreSave && filePath) {
-            const context: ContextT = this.fileMap[filePath][1];
-            const showSaveWarning: boolean | undefined = vscode.workspace.getConfiguration().get(this.showSavePromptKey);
-            if (showSaveWarning) {
-
-                const message: string = await this.getSaveConfirmationText(context);
-                const result: string | undefined = await vscode.window.showWarningMessage(message, DialogResponses.OK, DialogResponses.DontShowAgain);
-
-                if (!result) {
-                    throw new UserCancelledError();
-                } else if (result === DialogResponses.DontShowAgain) {
-                    await vscode.workspace.getConfiguration().update(this.showSavePromptKey, false, vscode.ConfigurationTarget.Global);
-                    await globalState.update(this.showSavePromptKey, true);
-                }
-            }
-
-            await this.updateRemote(context, doc);
-        }
-    }
-
+    // tslint:disable-next-line:member-ordering
     private static async writeToEditor(editor: vscode.TextEditor, data: string): Promise<void> {
         await editor.edit((editBuilder: vscode.TextEditorEdit) => {
             if (editor.document.lineCount > 0) {
-                const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+                const lastLine: vscode.TextLine = editor.document.lineAt(editor.document.lineCount - 1);
                 editBuilder.delete(new vscode.Range(new vscode.Position(0, 0), new vscode.Position(lastLine.range.start.line, lastLine.range.end.character)));
             }
 
--- ui/src/utils/TemporaryFile.ts
+++ ui/src/utils/TemporaryFile.ts
@@ -5,16 +5,19 @@
 
 import * as crypto from "crypto";
 import * as fse from 'fs-extra';
+import * as os from 'os';
 import * as path from 'path';
-import * as os from 'os'
 
+// tslint:disable-next-line:no-stateless-class export-name
 export class TemporaryFile {
-    private static randomFolderNameLength = 12;
-    
-    static async create(fileName: string): Promise<string> {
+    private static randomFolderNameLength: number = 12;
+
+    // tslint:disable-next-line:function-name
+    public static async create(fileName: string): Promise<string> {
         const buffer: Buffer = crypto.randomBytes(Math.ceil(TemporaryFile.randomFolderNameLength / 2));
-        var folderName = buffer.toString('hex').slice(0, TemporaryFile.randomFolderNameLength);
-        var filePath = path.join(os.tmpdir(), folderName, fileName);
+        const folderName: string = buffer.toString('hex').slice(0, TemporaryFile.randomFolderNameLength);
+        const filePath: string = path.join(os.tmpdir(), folderName, fileName);
+        // tslint:disable-next-line:no-unsafe-any
         await fse.ensureFile(filePath);
         return filePath;
     }
