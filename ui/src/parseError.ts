4dd7e428589b0552209d51d1c01deda61ea65e1a
Stephen Weatherford <Stephen.Weatherford@microsoft.com>
2018-01-05 17:17:04 -0800

    Handle errors that have message instead of body and other weirdness

--- ui/src/parseError.ts
+++ ui/src/parseError.ts
@@ -6,33 +6,38 @@
 import { IParsedError } from '../index';
 import { localize } from './localize';
 
-// tslint:disable-next-line:no-any
+// tslint:disable:no-unsafe-any
+// tslint:disable:no-any
 export function parseError(error: any): IParsedError {
-    let errorType: string;
-    let message: string;
-    if (error instanceof Error) {
-        try {
-            // Azure errors have a JSON object in the message
-            // tslint:disable-next-line:no-unsafe-any
-            errorType = JSON.parse(error.message).Code;
-            // tslint:disable-next-line:no-unsafe-any
-            message = JSON.parse(error.message).Message;
-        } catch (err) {
+    let errorType: string = '';
+    let message: string = '';
+
+    error = unpackErrorFromBody(error);
+
+    if (typeof (error) === 'object' && error !== null) {
+        if (error.constructor !== Object) {
             errorType = error.constructor.name;
-            message = error.message;
         }
-    } else if (typeof (error) === 'object' && error !== null) {
-        errorType = (<object>error).constructor.name;
-        message = JSON.stringify(error);
-        // tslint:disable-next-line:no-unsafe-any
+
+        errorType = getCode(error, errorType);
+        message = getMessage(error, message);
+
+        // Azure errors have a JSON object in the message
+        const parsedMessage: any = parseIfJson(error.message);
+        errorType = getCode(parsedMessage, errorType);
+        message = getMessage(parsedMessage, message);
+
+        message = message || JSON.stringify(error);
     } else if (error !== undefined && error !== null && error.toString && error.toString().trim() !== '') {
         errorType = typeof (error);
-        // tslint:disable-next-line:no-unsafe-any
         message = error.toString();
-    } else {
-        errorType = typeof (error);
-        message = localize('unknownError', 'Unknown Error');
     }
+
+    message = unpackErrorsInMessage(message);
+
+    // tslint:disable-next-line:strict-boolean-expressions
+    errorType = errorType || typeof (error);
+    message = message || localize('unknownError', 'Unknown Error');
 
     return {
         errorType: errorType,
@@ -41,4 +46,58 @@ export function parseError(error: any): IParsedError {
         // See https://github.com/Microsoft/vscode-azuretools/issues/51 for more info
         isUserCancelledError: errorType === 'UserCancelledError'
     };
+}
+
+function parseIfJson(o: any): any {
+    if (typeof o === 'string' && o.indexOf('{') >= 0) {
+        try {
+            return JSON.parse(o);
+        } catch {
+            // ignore
+        }
+    }
+
+    return o;
+}
+
+function getMessage(o: any, defaultMessage: string): string {
+    return (o && (o.message || o.Message)) || defaultMessage;
+}
+
+function getCode(o: any, defaultCode: string): string {
+    return (o && (o.code || o.Code || o.errorCode)) || defaultCode;
+}
+
+function unpackErrorsInMessage(message: string): string {
+    // Handle messages like this from Azure (just handle first error for now)
+    //   ["Errors":["The offer should have valid throughput бн",
+    if (message) {
+        const errorsInMessage: RegExpMatchArray | null = message.match(/"Errors":\[\s*"([^"]+)"/);
+        if (errorsInMessage !== null) {
+            const [, firstError] = errorsInMessage;
+            return firstError;
+        }
+    }
+
+    return message;
+}
+
+function unpackErrorFromBody(error: any): any {
+    // Handle objects from Azure SDK that contain the error information in a "body" field (serialized or not)
+    let body: any = error && error.body;
+    if (body) {
+        if (typeof body === 'string' && body.indexOf('{') >= 0) {
+            try {
+                body = JSON.parse(body);
+            } catch (err) {
+                // Ignore
+            }
+        }
+
+        if (typeof body === 'object') {
+            return body;
+        }
+    }
+
+    return error;
 }
