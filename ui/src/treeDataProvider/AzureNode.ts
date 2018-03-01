4ed2c212ba3d500d2e739af619d6d238069c07f0
Nathan <naturins@microsoft.com>
2018-03-01 14:20:57 -0800

    Update AzureNode.ts
    Remove extra slash

--- ui/src/treeDataProvider/AzureNode.ts
+++ ui/src/treeDataProvider/AzureNode.ts
@@ -90,7 +90,7 @@ export class AzureNode<T extends IAzureTreeItem = IAzureTreeItem> implements IAz
 
     public openInPortal(id?: string): void {
         id = id === undefined ? id : this.id;
-        (<(s: string) => void>opn)(`${this.environment.portalUrl}/${this.tenantId}/#resource/${id}`);
+        (<(s: string) => void>opn)(`${this.environment.portalUrl}/${this.tenantId}/#resource${id}`);
     }
 
     public includeInNodePicker(expectedContextValues: string[]): boolean {
