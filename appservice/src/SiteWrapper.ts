c404de18db24bb26fea9b88fe1d0824faa917211
Nathan <naturins@microsoft.com>
2018-03-01 14:29:16 -0800

    I, Nathan, solemnly swear, to never edit through GitHub (#95)

--- appservice/src/SiteWrapper.ts
+++ appservice/src/SiteWrapper.ts
@@ -140,13 +140,13 @@ export class SiteWrapper {
             await client.webApps.createOrUpdateSourceControlSlot(this.resourceGroup, this.name, siteSourceControl, this.slotName) :
             await client.webApps.createOrUpdateSourceControl(this.resourceGroup, this.name, siteSourceControl);
     }
-                                                                
-    public async syncRepository(client: WebSiteManagementClient) {
+
+    public async syncRepository(client: WebSiteManagementClient): Promise<void> {
         return this.slotName ?
             await client.webApps.syncRepositorySlot(this.resourceGroup, this.name, this.slotName) :
             await client.webApps.syncRepository(this.resourceGroup, this.name);
     }
-                                                                
+
     public async deleteSite(client: WebSiteManagementClient, outputChannel: vscode.OutputChannel): Promise<void> {
         const confirmMessage: string = localize('deleteConfirmation', 'Are you sure you want to delete "{0}"?', this.appName);
         if (await vscode.window.showWarningMessage(confirmMessage, DialogResponses.yes, DialogResponses.cancel) !== DialogResponses.yes) {
@@ -607,7 +607,7 @@ export class SiteWrapper {
             try {
                 // a resync will fix the first broken build
                 // https://github.com/projectkudu/kudu/issues/2277
-                this.syncRepository(client);
+                await this.syncRepository(client);
             } catch (error) {
                 const parsedError: IParsedError = parseError(error);
                 // The portal returns 200, but is expecting a 204 which causes it to throw an error even after a successful sync
@@ -622,7 +622,7 @@ export class SiteWrapper {
         const learnMore: string = localize('learnMore', 'Learn More');
         const setupGithub: string = localize('setupGithub', 'You must give Azure access to your GitHub account.');
         const input: string | undefined = await vscode.window.showErrorMessage(setupGithub, learnMore);
-        if (input === learnMOre) {
+        if (input === learnMore) {
             // tslint:disable-next-line:no-unsafe-any
             opn('https://aka.ms/B7g6sw');
         }
