4650aeff607419fe40f62a4873c7775587f34f31
Eric Jizba <erijiz@microsoft.com>
2018-03-19 16:22:28 -0700

    Leverage common steps

--- appservice/src/createAppService/SiteNameStep.ts
+++ appservice/src/createAppService/SiteNameStep.ts
@@ -3,38 +3,37 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { ResourceManagementClient } from 'azure-arm-resource';
-import { ResourceGroup } from 'azure-arm-resource/lib/resource/models';
-import { Subscription } from 'azure-arm-resource/lib/subscription/models';
 // tslint:disable-next-line:no-require-imports
 import WebSiteManagementClient = require('azure-arm-website');
-import { AppServicePlan, ResourceNameAvailability } from 'azure-arm-website/lib/models';
-import { ServiceClientCredentials } from 'ms-rest';
-import { AzureWizardStep, IAzureUserInput } from 'vscode-azureextensionui';
-import { uiUtils } from '../utils/uiUtils';
-import { getAppKindDisplayName } from './AppKind';
+import { ResourceNameAvailability } from 'azure-arm-website/lib/models';
+import { AzureNameStep, IAzureNamingRules, IAzureUserInput, resourceGroupNamingRules, ResourceGroupStep, storageAccountNamingRules, StorageAccountStep } from 'vscode-azureextensionui';
+import { AppKind, getAppKindDisplayName } from './AppKind';
+import { appServicePlanNamingRules, AppServicePlanStep } from './AppServicePlanStep';
 import { IAppServiceWizardContext } from './IAppServiceWizardContext';
 
-export class SiteNameStep extends AzureWizardStep<IAppServiceWizardContext> {
+export class SiteNameStep extends AzureNameStep<IAppServiceWizardContext> {
     public async prompt(wizardContext: IAppServiceWizardContext, ui: IAzureUserInput): Promise<IAppServiceWizardContext> {
-        const credentials: ServiceClientCredentials = wizardContext.credentials;
-        const subscription: Subscription = wizardContext.subscription;
-        const client: WebSiteManagementClient = new WebSiteManagementClient(credentials, subscription.subscriptionId);
-        let siteName: string;
-        siteName = await ui.showInputBox({
+        const client: WebSiteManagementClient = new WebSiteManagementClient(wizardContext.credentials, wizardContext.subscription.subscriptionId);
+        wizardContext.siteName = (await ui.showInputBox({
             prompt: `Enter a globally unique name for the new ${getAppKindDisplayName(wizardContext.appKind)}.`,
             validateInput: async (value: string): Promise<string | undefined> => {
                 value = value ? value.trim() : '';
                 const nameAvailability: ResourceNameAvailability = await client.checkNameAvailability(value, 'site');
                 if (!nameAvailability.nameAvailable) {
                     return nameAvailability.message;
+                } else {
+                    return undefined;
                 }
-                return undefined;
             }
-        });
+        })).trim();
 
-        wizardContext.websiteName = siteName;
-        wizardContext.relatedNameTask = this.generateRelatedName(wizardContext, siteName);
+        const namingRules: IAzureNamingRules[] = [resourceGroupNamingRules];
+        if (wizardContext.appKind === AppKind.functionapp) {
+            namingRules.push(storageAccountNamingRules);
+        } else {
+            namingRules.push(appServicePlanNamingRules);
+        }
+        wizardContext.relatedNameTask = this.generateRelatedName(wizardContext, wizardContext.siteName, namingRules);
 
         return wizardContext;
     }
@@ -43,62 +42,14 @@ export class SiteNameStep extends AzureWizardStep<IAppServiceWizardContext> {
         return wizardContext;
     }
 
-    protected async isNameAvailable(name: string, resourceGroups: ResourceGroup[], appServicePlans: AppServicePlan[]): Promise<boolean> {
-        if (resourceGroups.findIndex((rg: ResourceGroup) => rg.name.toLowerCase() === name.toLowerCase()) >= 0) {
-            return false;
+    protected async isNameAvailable(wizardContext: IAppServiceWizardContext, name: string): Promise<boolean> {
+        const tasks: Promise<boolean>[] = [ResourceGroupStep.isNameAvailable(wizardContext, name)];
+        if (wizardContext.appKind === AppKind.functionapp) {
+            tasks.push(StorageAccountStep.isNameAvailable(wizardContext, name));
+        } else {
+            tasks.push(AppServicePlanStep.isNameAvailable(wizardContext, name, name));
         }
 
-        if (appServicePlans.findIndex((asp: AppServicePlan) => asp.name.toLowerCase() === name.toLowerCase()) >= 0) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Get a suggested base name for resources related to a given site name
-     * @param siteName Site name
-     */
-    private async generateRelatedName(wizardContext: IAppServiceWizardContext, siteName: string): Promise<string> {
-        const credentials: ServiceClientCredentials = wizardContext.credentials;
-        const subscription: Subscription = wizardContext.subscription;
-        const resourceClient: ResourceManagementClient = new ResourceManagementClient(credentials, subscription.subscriptionId);
-        const webSiteClient: WebSiteManagementClient = new WebSiteManagementClient(credentials, subscription.subscriptionId);
-
-        const resourceGroupsTask: Promise<ResourceGroup[]> = uiUtils.listAll(resourceClient.resourceGroups, resourceClient.resourceGroups.list());
-        const plansTask: Promise<AppServicePlan[]> = uiUtils.listAll(webSiteClient.appServicePlans, webSiteClient.appServicePlans.list());
-
-        const [groups, plans]: [ResourceGroup[], AppServicePlan[]] = await Promise.all([resourceGroupsTask, plansTask]);
-
-        // Website names are limited to 60 characters, resource group names to 90, storage accounts to 24
-        // Storage accounts cannot have uppercase letters or hyphens and at least 3 characters.
-        // So restrict everything to: 3-24 charcters, lowercase and digits only.
-        const minLength: number = 3;
-        const maxLength: number = 24;
-
-        const preferredName: string = siteName.toLowerCase().replace(/[^0-9a-z]/g, '');
-
-        function generateSuffixedName(i: number): string {
-            const suffix: string = `${i}`;
-            const minUnsuffixedLength: number = minLength - suffix.length;
-            const maxUnsuffixedLength: number = maxLength - suffix.length;
-            const unsuffixedName: string = preferredName.slice(0, maxUnsuffixedLength) + 'zzz'.slice(0, Math.max(0, minUnsuffixedLength - preferredName.length));
-            return unsuffixedName + suffix;
-        }
-
-        if (await this.isNameAvailable(preferredName, groups, plans)) {
-            return preferredName;
-        }
-
-        let count: number = 2;
-        let isAvailable: boolean = false;
-        let newName: string;
-        while (!isAvailable) {
-            newName = generateSuffixedName(count);
-            isAvailable = await this.isNameAvailable(newName, groups, plans);
-            count += 1;
-        }
-
-        return newName;
+        return (await Promise.all(tasks)).every((v: boolean) => v);
     }
 }
--- appservice/src/createAppService/createAppService.ts
+++ appservice/src/createAppService/createAppService.ts
@@ -7,14 +7,12 @@ import { Subscription } from 'azure-arm-resource/lib/subscription/models';
 import { Site } from 'azure-arm-website/lib/models';
 import { ServiceClientCredentials } from 'ms-rest';
 import { OutputChannel } from 'vscode';
-import { AzureWizard, AzureWizardStep, IActionContext, IAzureUserInput } from 'vscode-azureextensionui';
+import { AzureWizard, AzureWizardStep, IActionContext, IAzureUserInput, LocationStep, ResourceGroupStep, StorageAccountStep } from 'vscode-azureextensionui';
 import { AppKind, WebsiteOS } from './AppKind';
 import { AppServicePlanStep } from './AppServicePlanStep';
 import { IAppServiceWizardContext } from './IAppServiceWizardContext';
-import { ResourceGroupStep } from './ResourceGroupStep';
 import { SiteNameStep } from './SiteNameStep';
 import { SiteStep } from './SiteStep';
-import { StorageAccountStep } from './StorageAccountStep';
 
 export async function createAppService(appKind: AppKind, websiteOS: WebsiteOS, outputChannel: OutputChannel, ui: IAzureUserInput, actionContext: IActionContext, credentials: ServiceClientCredentials, subscription: Subscription, showCreatingNode?: (label: string) => void): Promise<Site> {
     const steps: AzureWizardStep<IAppServiceWizardContext>[] = [];
@@ -28,6 +26,7 @@ export async function createAppService(appKind: AppKind, websiteOS: WebsiteOS, o
         default:
             steps.push(new AppServicePlanStep());
     }
+    steps.push(new LocationStep());
     steps.push(new SiteStep());
 
     let wizardContext: IAppServiceWizardContext = {
@@ -43,7 +42,7 @@ export async function createAppService(appKind: AppKind, websiteOS: WebsiteOS, o
     actionContext = actionContext || <IActionContext>{ properties: {}, measurements: {} };
     wizardContext = await wizard.prompt(actionContext, ui);
     if (showCreatingNode) {
-        showCreatingNode(wizardContext.websiteName);
+        showCreatingNode(wizardContext.siteName);
     }
     wizardContext = await wizard.execute(actionContext, outputChannel);
 
