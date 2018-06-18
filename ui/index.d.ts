74e15b8cf584c0d67c77d4782a2a2b22fc5996c7
Stephen Weatherford <Stephen.Weatherford@microsoft.com>
2018-06-18 13:03:44 -0700

    Fixes #186 and other sign-in issues

--- ui/index.d.ts
+++ ui/index.d.ts
@@ -616,3 +616,8 @@ export declare namespace DialogResponses {
     export const dontUpload: MessageItem;
     export const reportAnIssue: MessageItem;
 }
+
+/**
+ * Represents any that can be caught in a try/catch
+ */
+export type Catchable = any;
