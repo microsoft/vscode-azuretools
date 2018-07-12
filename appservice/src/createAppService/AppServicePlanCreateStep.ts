148abef6905ba35ad283f5ee4d3a38eec05f64f3
Nathan <naturins@microsoft.com>
2018-07-12 12:14:04 -0700

    ASP logic to look for asp before create (#223)
    * ASP logic to look for asp before create

--- appservice/src/createAppService/AppServicePlanCreateStep.ts
+++ appservice/src/createAppService/AppServicePlanCreateStep.ts
@@ -5,7 +5,7 @@
 
 // tslint:disable-next-line:no-require-imports
 import WebSiteManagementClient = require('azure-arm-website');
-import { SkuDescription } from 'azure-arm-website/lib/models';
+import { AppServicePlan, SkuDescription } from 'azure-arm-website/lib/models';
 import { ProgressLocation, window } from 'vscode';
 import { addExtensionUserAgent, AzureWizardExecuteStep } from 'vscode-azureextensionui';
 import { ext } from '../extensionVariables';
@@ -20,21 +20,32 @@ export class AppServicePlanCreateStep extends AzureWizardExecuteStep<IAppService
             const newPlanName: string = wizardContext.newPlanName!;
             // tslint:disable-next-line:no-non-null-assertion
             const newSku: SkuDescription = wizardContext.newPlanSku!;
-            const findingAppServicePlan: string = localize('FindingAppServicePlan', 'Ensuring App Service plan "{0}" with pricing tier "{1}" exists...', newPlanName, newSku.name);
+            const findingAppServicePlan: string = localize('FindingAppServicePlan', 'Ensuring App Service plan "{0}" exists...', newPlanName);
+            const creatingAppServicePlan: string = localize('CreatingAppServicePlan', 'Creating App Service plan "{0}"...', newPlanName);
             const foundAppServicePlan: string = localize('FoundAppServicePlan', 'Successfully found App Service plan "{0}".', newPlanName);
-            await window.withProgress({ location: ProgressLocation.Notification, title: findingAppServicePlan}, async (): Promise<void> => {
+            const createdAppServicePlan: string = localize('CreatedAppServicePlan', 'Successfully created App Service plan "{0}".', newPlanName);
+            await window.withProgress({ location: ProgressLocation.Notification, title: findingAppServicePlan }, async (): Promise<void> => {
                 ext.outputChannel.appendLine(findingAppServicePlan);
                 const client: WebSiteManagementClient = new WebSiteManagementClient(wizardContext.credentials, wizardContext.subscriptionId);
                 addExtensionUserAgent(client);
-                wizardContext.plan = await client.appServicePlans.createOrUpdate(wizardContext.resourceGroup.name, newPlanName, {
-                    appServicePlanName: newPlanName,
-                    kind: getAppServicePlanModelKind(wizardContext.newSiteKind, wizardContext.newSiteOS),
-                    sku: newSku,
-                    location: wizardContext.location.name,
-                    reserved: wizardContext.newSiteOS === WebsiteOS.linux  // The secret property - must be set to true to make it a Linux plan. Confirmed by the team who owns this API.
+                const existingPlan: AppServicePlan | null = await client.appServicePlans.get(wizardContext.resourceGroup.name, newPlanName);
+                if (existingPlan) {
+                    wizardContext.plan = existingPlan;
+                    window.showInformationMessage(foundAppServicePlan);
+                    ext.outputChannel.appendLine(foundAppServicePlan);
+                } else {
+                    ext.outputChannel.appendLine(creatingAppServicePlan);
+                    window.showInformationMessage(creatingAppServicePlan);
+                    wizardContext.plan = await client.appServicePlans.createOrUpdate(wizardContext.resourceGroup.name, newPlanName, {
+                        appServicePlanName: newPlanName,
+                        kind: getAppServicePlanModelKind(wizardContext.newSiteKind, wizardContext.newSiteOS),
+                        sku: newSku,
+                        location: wizardContext.location.name,
+                        reserved: wizardContext.newSiteOS === WebsiteOS.linux  // The secret property - must be set to true to make it a Linux plan. Confirmed by the team who owns this API.
                     });
-                window.showInformationMessage(foundAppServicePlan);
-                ext.outputChannel.appendLine(foundAppServicePlan);
+                    window.showInformationMessage(createdAppServicePlan);
+                    ext.outputChannel.appendLine(createdAppServicePlan);
+                }
             });
         }
 
