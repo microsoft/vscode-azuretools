2367ace5d6b045e05843471bd853aeee8eb2a7b9
Eric Jizba <erijiz@microsoft.com>
2018-07-12 11:58:26 -0700

    Move confirmDeployment to extensions

--- appservice/src/deploy/deployZip.ts
+++ appservice/src/deploy/deployZip.ts
@@ -5,7 +5,6 @@
 
 import * as fs from 'fs';
 import * as vscode from 'vscode';
-import { DialogResponses, TelemetryProperties } from 'vscode-azureextensionui';
 import KuduClient from 'vscode-azurekudu';
 import { ext } from '../extensionVariables';
 import * as FileUtilities from '../FileUtilities';
@@ -15,15 +14,7 @@ import { SiteClient } from '../SiteClient';
 import { formatDeployLog } from './formatDeployLog';
 import { waitForDeploymentToComplete } from './waitForDeploymentToComplete';
 
-export async function deployZip(client: SiteClient, fsPath: string, configurationSectionName: string, confirmDeployment: boolean, telemetryProperties?: TelemetryProperties): Promise<void> {
-    if (confirmDeployment) {
-        const warning: string = localize('zipWarning', 'Are you sure you want to deploy to "{0}"? This will overwrite any previous deployment and cannot be undone.', client.fullName);
-        telemetryProperties.cancelStep = 'confirmDestructiveDeployment';
-        const deploy: vscode.MessageItem = { title: localize('deploy', 'Deploy') };
-        await ext.ui.showWarningMessage(warning, { modal: true }, deploy, DialogResponses.cancel);
-        telemetryProperties.cancelStep = '';
-    }
-
+export async function deployZip(client: SiteClient, fsPath: string, configurationSectionName: string): Promise<void> {
     const kuduClient: KuduClient = await getKuduClient(client);
     let zipFilePath: string;
     let createdZip: boolean = false;
