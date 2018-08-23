e732a88e905661a904957d819b9a5243376a2aaf
Eric Jizba <erijiz@microsoft.com>
2018-08-23 16:53:57 -0700

    Move more log stream logic into shared package (#245)
    There are two bugs related to log streaming that affect both app service and functions. I moved some more of the logic into the shared package so that I could fix both of them here.

--- appservice/src/startStreamingLogs.ts
+++ appservice/src/startStreamingLogs.ts
@@ -9,6 +9,7 @@ import { setInterval } from 'timers';
 import * as vscode from 'vscode';
 import { callWithTelemetryAndErrorHandling, IActionContext, parseError } from 'vscode-azureextensionui';
 import KuduClient from 'vscode-azurekudu';
+import { ext } from './extensionVariables';
 import { getKuduClient } from './getKuduClient';
 import { localize } from './localize';
 import { pingFunctionApp } from './pingFunctionApp';
@@ -17,68 +18,98 @@ import { SiteClient } from './SiteClient';
 
 export interface ILogStream extends vscode.Disposable {
     isConnected: boolean;
+    outputChannel: vscode.OutputChannel;
 }
 
-/**
- * Starts the log-streaming service. Call 'dispose()' on the returned object when you want to stop the service.
- */
-export async function startStreamingLogs(client: SiteClient, outputChannel: vscode.OutputChannel, path: string = ''): Promise<ILogStream> {
-    const kuduClient: KuduClient = await getKuduClient(client);
+const logStreams: Map<string, ILogStream> = new Map();
 
-    outputChannel.show();
-    outputChannel.appendLine(localize('connectingToLogStream', 'Connecting to log stream...'));
-    const httpRequest: WebResource = new WebResource();
-    await signRequest(httpRequest, kuduClient.credentials);
+function getLogStreamId(client: SiteClient, logsPath: string): string {
+    return `${client.id}${logsPath}`;
+}
 
-    const requestApi: request.RequestAPI<request.Request, request.CoreOptions, {}> = request.defaults(httpRequest);
-    return await new Promise((onLogStreamCreated: (logStream: ILogStream) => void): void => {
-        // Intentionally setting up a separate telemetry event and not awaiting the result here since log stream is a long-running action
-        // tslint:disable-next-line:no-floating-promises
-        callWithTelemetryAndErrorHandling('appService.streamingLogs', async function (this: IActionContext): Promise<void> {
-            this.suppressErrorDisplay = true;
-            let timerId: NodeJS.Timer | undefined;
-            if (client.isFunctionApp) {
-                // For Function Apps, we have to ping "/admin/host/status" every minute for logging to work
-                // https://github.com/Microsoft/vscode-azurefunctions/issues/227
-                await pingFunctionApp(client);
-                timerId = setInterval(async () => await pingFunctionApp(client), 60 * 1000);
-            }
+export async function startStreamingLogs(client: SiteClient, verifyLoggingEnabled: () => Promise<void>, logStreamLabel: string, logsPath: string = ''): Promise<ILogStream> {
+    const logStreamId: string = getLogStreamId(client, logsPath);
+    const logStream: ILogStream | undefined = logStreams.get(logStreamId);
 
-            await new Promise((onLogStreamEnded: () => void, reject: (err: Error) => void): void => {
-                let logStream: ILogStream;
-                const logsRequest: request.Request = requestApi(`${client.kuduUrl}/api/logstream/${path}`);
-                logStream = {
-                    dispose: (): void => {
-                        logsRequest.removeAllListeners();
-                        logsRequest.destroy();
-                        outputChannel.show();
+    if (logStream && logStream.isConnected) {
+        logStream.outputChannel.show();
+        await ext.ui.showWarningMessage(localize('logStreamAlreadyActive', 'The log-streaming service for "{0}" is already active.', logStreamLabel));
+        return logStream;
+    } else {
+        await verifyLoggingEnabled();
+
+        const outputChannel: vscode.OutputChannel = logStream ? logStream.outputChannel : vscode.window.createOutputChannel(localize('logStreamLabel', '{0} - Log Stream', logStreamLabel));
+        ext.context.subscriptions.push(outputChannel);
+
+        const kuduClient: KuduClient = await getKuduClient(client);
+
+        outputChannel.show();
+        outputChannel.appendLine(localize('connectingToLogStream', 'Connecting to log stream...'));
+        const httpRequest: WebResource = new WebResource();
+        await signRequest(httpRequest, kuduClient.credentials);
+
+        const requestApi: request.RequestAPI<request.Request, request.CoreOptions, {}> = request.defaults(httpRequest);
+        return await new Promise((onLogStreamCreated: (ls: ILogStream) => void): void => {
+            // Intentionally setting up a separate telemetry event and not awaiting the result here since log stream is a long-running action
+            // tslint:disable-next-line:no-floating-promises
+            callWithTelemetryAndErrorHandling('appService.streamingLogs', async function (this: IActionContext): Promise<void> {
+                this.suppressErrorDisplay = true;
+                let timerId: NodeJS.Timer | undefined;
+                if (client.isFunctionApp) {
+                    // For Function Apps, we have to ping "/admin/host/status" every minute for logging to work
+                    // https://github.com/Microsoft/vscode-azurefunctions/issues/227
+                    await pingFunctionApp(client);
+                    timerId = setInterval(async () => await pingFunctionApp(client), 60 * 1000);
+                }
+
+                await new Promise((onLogStreamEnded: () => void, reject: (err: Error) => void): void => {
+                    let newLogStream: ILogStream;
+                    const logsRequest: request.Request = requestApi(`${client.kuduUrl}/api/logstream/${logsPath}`);
+                    newLogStream = {
+                        dispose: (): void => {
+                            logsRequest.removeAllListeners();
+                            logsRequest.destroy();
+                            outputChannel.show();
+                            if (timerId) {
+                                clearInterval(timerId);
+                            }
+                            outputChannel.appendLine(localize('logStreamDisconnected', 'Disconnected from log-streaming service.'));
+                            newLogStream.isConnected = false;
+                            onLogStreamEnded();
+                        },
+                        isConnected: true,
+                        outputChannel: outputChannel
+                    };
+
+                    logsRequest.on('data', (chunk: Buffer | string) => {
+                        outputChannel.appendLine(chunk.toString());
+                    }).on('error', (err: Error) => {
                         if (timerId) {
                             clearInterval(timerId);
                         }
-                        outputChannel.appendLine(localize('logStreamDisconnected', 'Disconnected from log-streaming service.'));
-                        logStream.isConnected = false;
-                        onLogStreamEnded();
-                    },
-                    isConnected: true
-                };
+                        newLogStream.isConnected = false;
+                        outputChannel.show();
+                        outputChannel.appendLine(localize('logStreamError', 'Error connecting to log-streaming service:'));
+                        outputChannel.appendLine(parseError(err).message);
+                        reject(err);
+                    }).on('complete', () => {
+                        newLogStream.dispose();
+                    });
 
-                logsRequest.on('data', (chunk: Buffer | string) => {
-                    outputChannel.appendLine(chunk.toString());
-                }).on('error', (err: Error) => {
-                    if (timerId) {
-                        clearInterval(timerId);
-                    }
-                    logStream.isConnected = false;
-                    outputChannel.show();
-                    outputChannel.appendLine(localize('logStreamError', 'Error connecting to log-streaming service:'));
-                    outputChannel.appendLine(parseError(err).message);
-                    reject(err);
-                }).on('complete', () => {
-                    logStream.dispose();
+                    logStreams.set(logStreamId, newLogStream);
+                    onLogStreamCreated(newLogStream);
                 });
-
-                onLogStreamCreated(logStream);
             });
         });
-    });
+    }
+}
+
+export async function stopStreamingLogs(client: SiteClient, logsPath: string = ''): Promise<void> {
+    const logStreamId: string = getLogStreamId(client, logsPath);
+    const logStream: ILogStream | undefined = logStreams.get(logStreamId);
+    if (logStream && logStream.isConnected) {
+        logStream.dispose();
+    } else {
+        await vscode.window.showWarningMessage(localize('alreadyActive', 'The log-streaming service for "{0}" is already disconnected.', logStream.outputChannel.name));
+    }
 }
