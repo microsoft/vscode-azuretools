a28a28d6772d79b9a89ae0f5dc23d0c08ee09472
Eric Jizba <erijiz@microsoft.com>
2018-09-11 14:30:43 -0700

    Add several function-specific ARM methods to SiteClient

--- appservice/src/pingFunctionApp.ts
+++ appservice/src/pingFunctionApp.ts
@@ -5,16 +5,12 @@
 
 import { TokenCredentials, WebResource } from 'ms-rest';
 import * as requestP from 'request-promise';
-import KuduClient from 'vscode-azurekudu';
-import { getKuduClient } from './getKuduClient';
 import { signRequest } from './signRequest';
 import { SiteClient } from './SiteClient';
 
 export async function pingFunctionApp(client: SiteClient): Promise<void> {
-    const kuduClient: KuduClient = await getKuduClient(client);
-
     const requestOptions: WebResource = new WebResource();
-    const adminKey: string = await kuduClient.functionModel.getAdminToken();
+    const adminKey: string = await client.getFunctionsAdminToken();
     await signRequest(requestOptions, new TokenCredentials(adminKey));
     // tslint:disable-next-line:no-unsafe-any
     await requestP.get(`${client.defaultHostUrl}/admin/host/status`, requestOptions);
