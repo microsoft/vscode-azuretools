b13806aa3b9a47e681cdb27c801edfb52a9998c4
Eric Jizba <erijiz@microsoft.com>
2018-11-06 13:29:40 -0800

    Add ApiVersionCode (#303)
    That way extensions can add custom handling if they want to.

--- ui/test/apiProvider.test.ts
+++ ui/test/apiProvider.test.ts
@@ -5,7 +5,9 @@
 
 import * as assert from 'assert';
 import { AzureExtensionApi, AzureExtensionApiProvider } from '../api';
-import { wrapApiWithVersioning } from '../src/wrapApiWithVersioning';
+import { IParsedError } from '../index';
+import { createApiProvider } from '../src/createApiProvider';
+import { parseError } from '../src/parseError';
 import { assertThrowsAsync } from './assertThrowsAsync';
 
 class TestApi implements AzureExtensionApi {
@@ -39,28 +41,37 @@ class TestApi implements AzureExtensionApi {
 
 suite('AzureExtensionApiProvider tests', () => {
     test('Versioning', async () => {
-        assert.throws(() => wrapApiWithVersioning([new TestApi('invalidVersion')]), /Invalid semver/);
+        assert.throws(() => createApiProvider([new TestApi('invalidVersion')]), /Invalid semver/);
 
         const api1: TestApi = new TestApi('1.0.0');
         const api11: TestApi = new TestApi('1.1.0');
         const api111: TestApi = new TestApi('1.1.1');
         const api12: TestApi = new TestApi('1.2.0');
-        const apiProvider: AzureExtensionApiProvider = wrapApiWithVersioning([api1, api111, api11, api12]);
+        const apiProvider: AzureExtensionApiProvider = createApiProvider([api1, api111, api11, api12]);
 
-        assert.throws(() => apiProvider.getApi('0.1'), /no longer supported/);
-        assert.throws(() => apiProvider.getApi('1.1.2'), /must be updated/);
-        assert.throws(() => apiProvider.getApi('2'), /must be updated/);
+        assert.throws(() => apiProvider.getApi('0.1'), (error) => validateApiError(error, /no longer supported/, 'NoLongerSupported'));
+        assert.throws(() => apiProvider.getApi('1.1.2'), (error) => validateApiError(error, /must be updated/, 'NotYetSupported'));
+        assert.throws(() => apiProvider.getApi('2'), (error) => validateApiError(error, /must be updated/, 'NotYetSupported'));
 
         const latestApi12: TestApi = apiProvider.getApi<TestApi>('1');
         assert.equal(latestApi12.apiVersion, '1.2.0');
 
         const latestApi11: TestApi = apiProvider.getApi<TestApi>('1.1');
         assert.equal(latestApi11.apiVersion, '1.1.1');
+
+        const latestApi11Carot: TestApi = apiProvider.getApi<TestApi>('^1.1.0');
+        assert.equal(latestApi11Carot.apiVersion, '1.2.0');
+
+        const latestApi11Tilde: TestApi = apiProvider.getApi<TestApi>('~1.1.0');
+        assert.equal(latestApi11Tilde.apiVersion, '1.1.1');
+
+        const emptyApiProvider: AzureExtensionApiProvider = createApiProvider([]);
+        assert.throws(() => emptyApiProvider.getApi('^1.0.0'), (error) => validateApiError(error, /must be updated/, 'NotYetSupported'));
     });
 
     test('Wrapped api is same as original api', async () => {
         const api: TestApi = new TestApi('1.0.0');
-        const apiProvider: AzureExtensionApiProvider = wrapApiWithVersioning([api]);
+        const apiProvider: AzureExtensionApiProvider = createApiProvider([api]);
 
         const wrappedApi: TestApi = apiProvider.getApi<TestApi>('1');
 
@@ -72,3 +83,9 @@ suite('AzureExtensionApiProvider tests', () => {
         await assertThrowsAsync(wrappedApi.testFuncErrorAsync, /testFuncErrorAsync/);
     });
 });
+
+// tslint:disable-next-line:no-any
+function validateApiError(error: any, regexp: RegExp, expectedCode: string): boolean {
+    const errorInfo: IParsedError = parseError(error);
+    return regexp.test(errorInfo.message) && errorInfo.errorType === expectedCode;
+}
