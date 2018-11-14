04d5acd8b9dc56b5a1878292445ffd2628317f6e
Nathan <naturins@microsoft.com>
2018-11-14 15:22:41 -0800

    Deployments implementation (#314)
    Add deployments and deployment treeItems and implementation of viewing/redeploying

---
+++ appservice/src/tree/DeploymentTreeItem.ts
@@ -0,0 +1,100 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as os from 'os';
+import * as path from 'path';
+import { ProgressLocation, TextDocument, window, workspace } from 'vscode';
+import { AzureTreeItem } from 'vscode-azureextensionui';
+import KuduClient from 'vscode-azurekudu';
+import { DeployResult, LogEntry } from 'vscode-azurekudu/lib/models';
+import { ext } from '../extensionVariables';
+import { getKuduClient } from '../getKuduClient';
+import { nonNullProp } from '../utils/nonNull';
+import { DeploymentsTreeItem } from './DeploymentsTreeItem';
+import { ISiteTreeRoot } from './ISiteTreeRoot';
+
+export class DeploymentTreeItem extends AzureTreeItem<ISiteTreeRoot> {
+    public static contextValue: string = 'deployment';
+    public readonly contextValue: string = DeploymentTreeItem.contextValue;
+    public label: string;
+    public receivedTime: Date;
+    public parent: DeploymentsTreeItem;
+    private _deployResult: DeployResult;
+
+    constructor(parent: DeploymentsTreeItem, deployResult: DeployResult) {
+        super(parent);
+        this._deployResult = deployResult;
+        this.receivedTime = nonNullProp(deployResult, 'receivedTime');
+        const active: boolean = nonNullProp(deployResult, 'active');
+        let message: string = nonNullProp(deployResult, 'message');
+        if (message.length > 50) { /* truncate long messages and add "..." */
+            message = `${message.substring(0, 50)}...`;
+        }
+        this.label = `${this.id.substring(0, 7)} - ${message}`;
+
+        if (active) {
+            this.description = 'Active';
+        } else if (!this._deployResult.complete) {
+            this.description = 'Deploying...';
+        } else if (!this._deployResult.lastSuccessEndTime) {
+            this.description = 'Failed';
+        }
+    }
+
+    public get iconPath(): { light: string, dark: string } {
+        return {
+            light: path.join(__filename, '..', '..', '..', 'resources', 'light', 'Git_Commit_16x.svg'),
+            dark: path.join(__filename, '..', '..', '..', 'resources', 'dark', 'Git_Commit_16x.svg')
+        };
+    }
+
+    public get id(): string {
+        this._deployResult.id = nonNullProp(this._deployResult, 'id');
+        return this._deployResult.id;
+    }
+
+    public async redeployDeployment(): Promise<void> {
+        const redeploying: string = `Redeploying commit "${this.id}" to "${this.root.client.fullName}"`;
+        const deployed: string = `Commit "${this.id}" has been redeployed to "${this.root.client.fullName}".`;
+        window.withProgress({ location: ProgressLocation.Notification, title: redeploying }, async (): Promise<void> => {
+            const kuduClient: KuduClient = await getKuduClient(this.root.client);
+            ext.outputChannel.appendLine(redeploying);
+            await kuduClient.deployment.deploy(this.id);
+            await this.parent.refresh();
+            window.showInformationMessage(deployed);
+            ext.outputChannel.appendLine(deployed);
+        });
+    }
+
+    public async getDeploymentLogs(): Promise<string> {
+        const kuduClient: KuduClient = await getKuduClient(this.root.client);
+        const logEntries: LogEntry[] = await kuduClient.deployment.getLogEntry(this.id);
+        let data: string = '';
+        for (const logEntry of logEntries) {
+            data += this.formatLogEntry(logEntry);
+            if (logEntry.detailsUrl && logEntry.id) {
+                const detailedLogEntries: LogEntry[] = await kuduClient.deployment.getLogEntryDetails(this.id, logEntry.id);
+                for (const detailedEntry of detailedLogEntries) {
+                    data += this.formatLogEntry(detailedEntry);
+                }
+            }
+        }
+        return data;
+    }
+
+    public async viewDeploymentLogs(): Promise<void> {
+        const logData: string = await this.getDeploymentLogs();
+        const logDocument: TextDocument = await workspace.openTextDocument({ content: logData, language: 'log' });
+        await window.showTextDocument(logDocument);
+    }
+
+    private formatLogEntry(logEntry: LogEntry): string {
+        if (logEntry.logTime && logEntry.message) {
+            return `${logEntry.logTime.toISOString()} - ${logEntry.message}${os.EOL}`;
+        } else {
+            return '';
+        }
+    }
+}
---
+++ appservice/src/tree/DeploymentsTreeItem.ts
@@ -0,0 +1,89 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { SiteConfig } from 'azure-arm-website/lib/models';
+import * as path from 'path';
+import { AzureParentTreeItem, createTreeItemsWithErrorHandling, DialogResponses, GenericTreeItem, IActionContext } from 'vscode-azureextensionui';
+import KuduClient from 'vscode-azurekudu';
+import { DeployResult } from 'vscode-azurekudu/lib/models';
+import { editScmType } from '../editScmType';
+import { ext } from '../extensionVariables';
+import { getKuduClient } from '../getKuduClient';
+import { localize } from '../localize';
+import { ScmType } from '../ScmType';
+import { DeploymentTreeItem } from './DeploymentTreeItem';
+import { ISiteTreeRoot } from './ISiteTreeRoot';
+
+export class DeploymentsTreeItem extends AzureParentTreeItem<ISiteTreeRoot> {
+    public static contextValueConnected: string = 'deploymentsConnected';
+    public static contextValueUnconnected: string = 'deploymentsUnconnected';
+    public contextValue: string;
+    public parent: AzureParentTreeItem<ISiteTreeRoot>;
+    public readonly label: string = localize('Deployments', 'Deployments');
+    public readonly childTypeLabel: string = localize('Deployment', 'Deployment');
+
+    public constructor(parent: AzureParentTreeItem<ISiteTreeRoot>, siteConfig: SiteConfig) {
+        super(parent);
+        this.contextValue = siteConfig.scmType === ScmType.None ? DeploymentsTreeItem.contextValueUnconnected : DeploymentsTreeItem.contextValueConnected;
+    }
+
+    public get iconPath(): { light: string, dark: string } {
+        return {
+            light: path.join(__filename, '..', '..', '..', 'resources', 'light', 'Deployments_x16.svg'),
+            dark: path.join(__filename, '..', '..', '..', 'resources', 'dark', 'Deployments_x16.svg')
+        };
+    }
+
+    public hasMoreChildrenImpl(): boolean {
+        return false;
+    }
+
+    public async loadMoreChildrenImpl(_clearCache: boolean): Promise<DeploymentTreeItem[] | GenericTreeItem<ISiteTreeRoot>[]> {
+        const siteConfig: SiteConfig = await this.root.client.getSiteConfig();
+        if (siteConfig.scmType === ScmType.GitHub || siteConfig.scmType === ScmType.LocalGit) {
+            const kuduClient: KuduClient = await getKuduClient(this.root.client);
+            const deployments: DeployResult[] = await kuduClient.deployment.getDeployResults();
+            return await createTreeItemsWithErrorHandling(
+                this,
+                deployments,
+                'invalidDeployment',
+                (dr: DeployResult) => {
+                    return new DeploymentTreeItem(this, dr);
+                },
+                (dr: DeployResult) => {
+                    return dr.id ? dr.id.substring(0, 7) : undefined;
+                }
+            );
+        } else {
+            return [new GenericTreeItem(this, {
+                commandId: 'appService.ConnectToGitHub',
+                contextValue: 'ConnectToGithub',
+                label: 'Connect to a GitHub repository...'
+            })];
+        }
+    }
+
+    public compareChildrenImpl(ti1: DeploymentTreeItem, ti2: DeploymentTreeItem): number {
+        // sorts in accordance of the most recent deployment
+        return ti2.receivedTime.valueOf() - ti1.receivedTime.valueOf();
+    }
+
+    public async disconnectRepo(context: IActionContext): Promise<void> {
+        const disconnect: string = 'Disconnect from current repository?  You can reconnect at anytime.';
+        await ext.ui.showWarningMessage(disconnect, DialogResponses.yes, DialogResponses.cancel);
+        await editScmType(this.root.client, this.parent, context, ScmType.None);
+        await this.refresh();
+    }
+
+    public async refreshLabelImpl(): Promise<void> {
+        const siteConfig: SiteConfig = await this.root.client.getSiteConfig();
+        // while this doesn't directly refresh the label, it's currently the only place to run async code on refresh
+        if (siteConfig.scmType === ScmType.GitHub || siteConfig.scmType === ScmType.LocalGit) {
+            this.contextValue = DeploymentsTreeItem.contextValueConnected;
+        } else {
+            this.contextValue = DeploymentsTreeItem.contextValueUnconnected;
+        }
+    }
+}
