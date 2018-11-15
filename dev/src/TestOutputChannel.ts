57c8dcbfa019db1c4e94b28ab567c8df8c1f34b3
Eric Jizba <erijiz@microsoft.com>
2018-11-14 16:48:31 -0800

    Add TestOutputChannel to dev package

---
+++ dev/src/TestOutputChannel.ts
@@ -0,0 +1,34 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { OutputChannel } from "vscode";
+
+export class TestOutputChannel implements OutputChannel {
+    public name: string = 'Extension Test Output';
+
+    public append(value: string): void {
+        console.log(value);
+    }
+
+    public appendLine(value: string): void {
+        console.log(value);
+    }
+
+    public clear(): void {
+        // do nothing
+    }
+
+    public show(): void {
+        // do nothing
+    }
+
+    public hide(): void {
+        // do nothing
+    }
+
+    public dispose(): void {
+        // do nothing
+    }
+}
