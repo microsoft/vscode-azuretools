5f10981f5114df01156ecfd1d55613018c529810
nturinski <naturins@microsoft.com>
2018-11-29 14:27:29 -0800

    PR fixes

--- appservice/src/tree/DeploymentTreeItem.ts
+++ appservice/src/tree/DeploymentTreeItem.ts
@@ -81,7 +81,6 @@ export class DeploymentTreeItem extends AzureTreeItem<ISiteTreeRoot> {
     public async redeployDeployment(): Promise<void> {
         const redeploying: string = localize('redeploying', 'Redeploying commit "{0}" to "{1}". Check output window for status.', this.id, this.root.client.fullName);
         const redeployed: string = localize('redeployed', 'Commit "{0}" has been redeployed to "{1}".', this.id, this.root.client.fullName);
-        const timeout: string = localize('redeployTimeout', 'Redeploying commit "{0}" was unable to resolve and has timed out.', this.id);
         await window.withProgress({ location: ProgressLocation.Notification, title: redeploying }, async (): Promise<void> => {
             const kuduClient: KuduClient = await getKuduClient(this.root.client);
             const refreshingInteveral: NodeJS.Timer = setInterval(async () => { await this.refresh(); }, 1000); /* the status of the label changes during deployment so poll for that*/
@@ -96,9 +95,11 @@ export class DeploymentTreeItem extends AzureTreeItem<ISiteTreeRoot> {
                                 resolve();
                             }
                         },
-                        1000
+                        3000
                     );
-                    setTimeout(() => reject(new Error(timeout)), 5000);
+                    const timeout: string = localize('redeployTimeout', 'Redeploying commit "{0}" was unable to resolve and has timed out.', this.id);
+                    // a 20 second timeout period to let Kudu initialize the deployment
+                    setTimeout(() => reject(new Error(timeout)), 20000);
                 });
                 await waitForDeploymentToComplete(this.root.client, kuduClient);
                 await this.parent.refresh(); /* refresh entire node because active statuses has changed */
