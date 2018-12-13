6e47789710fadff9a706ba2dae01aadf009ad426
nturinski <naturins@microsoft.com>
2018-12-12 16:41:52 -0800

    Add confirmOverwrite to AppSettingsTreeItem as it used by Web Apps and Func

--- appservice/src/tree/AppSettingsTreeItem.ts
+++ appservice/src/tree/AppSettingsTreeItem.ts
@@ -5,7 +5,8 @@
 
 import { StringDictionary } from 'azure-arm-website/lib/models';
 import * as path from 'path';
-import { AzureParentTreeItem, AzureTreeItem } from 'vscode-azureextensionui';
+import { MessageItem } from 'vscode';
+import { AzureParentTreeItem, AzureTreeItem, DialogResponses } from 'vscode-azureextensionui';
 import { ext } from '../extensionVariables';
 import { AppSettingTreeItem } from './AppSettingTreeItem';
 import { ISiteTreeRoot } from './ISiteTreeRoot';
@@ -118,5 +119,76 @@ export class AppSettingsTreeItem extends AzureParentTreeItem<ISiteTreeRoot> {
         }
 
         return <StringDictionary>this._settings;
+    }
+
+    public async confirmOverwriteSettings(sourceSettings: { [key: string]: string }, destinationSettings: { [key: string]: string }, destinationName: string): Promise<void> {
+        function logKey(key: string): void {
+            ext.outputChannel.appendLine(`- ${key}`);
+        }
+        let suppressPrompt: boolean = false;
+        let overwriteSetting: boolean = false;
+
+        const addedKeys: string[] = [];
+        const updatedKeys: string[] = [];
+        const userIgnoredKeys: string[] = [];
+        const matchingKeys: string[] = [];
+
+        for (const key of Object.keys(sourceSettings)) {
+            if (destinationSettings[key] === undefined) {
+                addedKeys.push(key);
+                destinationSettings[key] = sourceSettings[key];
+            } else if (destinationSettings[key] !== sourceSettings[key]) {
+                if (!suppressPrompt) {
+                    const yesToAll: MessageItem = { title: 'Yes to all' };
+                    const noToAll: MessageItem = { title: 'No to all' };
+                    const message: string = `Setting "${key}" already exists in "${destinationName}". Overwrite?`;
+                    const result: MessageItem = await ext.ui.showWarningMessage(message, { modal: true }, DialogResponses.yes, yesToAll, DialogResponses.no, noToAll);
+                    if (result === DialogResponses.yes) {
+                        overwriteSetting = true;
+                    } else if (result === yesToAll) {
+                        overwriteSetting = true;
+                        suppressPrompt = true;
+                    } else if (result === DialogResponses.no) {
+                        overwriteSetting = false;
+                    } else if (result === noToAll) {
+                        overwriteSetting = false;
+                        suppressPrompt = true;
+                    }
+                }
+
+                if (overwriteSetting) {
+                    updatedKeys.push(key);
+                    destinationSettings[key] = sourceSettings[key];
+                } else {
+                    userIgnoredKeys.push(key);
+                }
+            } else {
+                matchingKeys.push(key);
+            }
+        }
+
+        if (addedKeys.length > 0) {
+            ext.outputChannel.appendLine('Added the following settings:');
+            addedKeys.forEach(logKey);
+        }
+
+        if (updatedKeys.length > 0) {
+            ext.outputChannel.appendLine('Updated the following settings:');
+            updatedKeys.forEach(logKey);
+        }
+
+        if (matchingKeys.length > 0) {
+            ext.outputChannel.appendLine('Ignored the following settings that were already the same:');
+            matchingKeys.forEach(logKey);
+        }
+
+        if (userIgnoredKeys.length > 0) {
+            ext.outputChannel.appendLine('Ignored the following settings based on user input:');
+            userIgnoredKeys.forEach(logKey);
+        }
+
+        if (Object.keys(destinationSettings).length > Object.keys(sourceSettings).length) {
+            ext.outputChannel.appendLine(`WARNING: This operation will not delete any settings in "${destinationName}". You must manually delete settings if desired.`);
+        }
     }
 }
