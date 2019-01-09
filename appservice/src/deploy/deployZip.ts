746a6899233eaf4fd0f9c2ad3657db41997ef9ce
Nathan <naturins@microsoft.com>
2019-01-09 12:06:17 -0800

    Add 10 second delay to basic asp app's first deployment (#362)
    For the first deployment on a Basic asp Linux Node app, delay for 10 seconds to allow for the container to finish resetting.

--- appservice/src/deploy/deployZip.ts
+++ appservice/src/deploy/deployZip.ts
@@ -37,13 +37,17 @@ export async function deployZip(client: SiteClient, fsPath: string, configuratio
         const asp: AppServicePlan | undefined = await aspPromise;
         // Assume it's consumption if we can't get the plan (sometimes happens with brand new plans). Consumption is recommended and more popular for functions
         const isConsumption: boolean = !asp || (!!asp.sku && !!asp.sku.tier && asp.sku.tier.toLowerCase() === 'dynamic');
-        if (client.kind.toLowerCase().includes('linux') && isConsumption) {
+        if (client.isLinux && isConsumption) {
             // Linux consumption doesn't support kudu zipPushDeploy
             await deployToStorageAccount(client, zipFilePath);
         } else {
             const kuduClient: KuduClient = await getKuduClient(client);
             await kuduClient.pushDeployment.zipPushDeploy(fs.createReadStream(zipFilePath), { isAsync: true });
             await waitForDeploymentToComplete(client, kuduClient);
+            // https://github.com/Microsoft/vscode-azureappservice/issues/644
+            // This delay is a temporary stopgap that should be resolved with the new pipelines
+            await delayFirstWebAppDeploy(client, asp, kuduClient);
+
         }
     } finally {
         if (createdZip) {
@@ -61,4 +65,30 @@ async function getZipFileToDeploy(fsPath: string, configurationSectionName?: str
     } else {
         return FileUtilities.zipFile(fsPath);
     }
+}
+
+async function delayFirstWebAppDeploy(client: SiteClient, asp: AppServicePlan | undefined, kuduClient: KuduClient): Promise<void> {
+    await new Promise<void>(async (resolve: () => void): Promise<void> => {
+        setTimeout(resolve, 10000);
+        try {
+            // this delay is only valid for the first deployment to a Linux web app on a basic asp, so resolve for anything else
+            if (client.isFunctionApp) {
+                resolve();
+            }
+            if (!asp || !asp.sku || !asp.sku.tier || asp.sku.tier.toLowerCase() !== 'basic') {
+                resolve();
+            }
+            if (!client.isLinux) {
+                resolve();
+            }
+
+            const deployments: number = (await kuduClient.deployment.getDeployResults()).length;
+            if (deployments > 1) {
+                resolve();
+            }
+        } catch (error) {
+            // ignore the error, an error here isn't a deployment failure
+            resolve();
+        }
+    });
 }
