2b2e0c999b7b18f0772e320e058f4a48c085d333
nturinski <naturins@microsoft.com>
2019-01-09 11:47:58 -0800

    Fix some indenting

--- appservice/src/deploy/deployZip.ts
+++ appservice/src/deploy/deployZip.ts
@@ -68,24 +68,23 @@ async function getZipFileToDeploy(fsPath: string, configurationSectionName?: str
 }
 
 async function delayFirstWebAppDeploy(client: SiteClient, asp: AppServicePlan | undefined, kuduClient: KuduClient): Promise<void> {
-        await new Promise<void>(async (resolve: () => void): Promise<void> => {
-            setTimeout(resolve, 10000);
+    await new Promise<void>(async (resolve: () => void): Promise<void> => {
+        setTimeout(resolve, 10000);
+        try {
+            // this delay is only valid for the first deployment to a Linux web app on a basic asp, so resolve for anything else
+            if (client.isFunctionApp) {
+                resolve();
+            }
+            if (!asp || !asp.sku || !asp.sku.tier || asp.sku.tier.toLowerCase() !== 'basic') {
+                resolve();
+            }
+            if (!client.isLinux) {
+                resolve();
+            }
 
-            try {
-                // this delay is only valid for the first deployment to a Linux web app on a basic asp, so resolve for anything else
-                if (client.isFunctionApp) {
-                    resolve();
-                }
-                if (!asp || !asp.sku || !asp.sku.tier || asp.sku.tier.toLowerCase() !== 'basic') {
-                    resolve();
-                }
-                if (!client.isLinux) {
-                    resolve();
-                }
-
-                const deployments: number = (await kuduClient.deployment.getDeployResults()).length;
-                if (deployments > 1) {
-                    resolve();
+            const deployments: number = (await kuduClient.deployment.getDeployResults()).length;
+            if (deployments > 1) {
+                resolve();
             }
         } catch (error) {
             // ignore the error, an error here isn't a deployment failure
