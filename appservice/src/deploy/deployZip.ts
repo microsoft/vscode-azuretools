597e8461c738ec9f483a3c6d067d791d74b67f94
Eric Jizba <erijiz@microsoft.com>
2019-02-15 11:51:04 -0800

    Add support for .funcignore (#415)
    .funcignore is already supported by the func cli and uses .gitignore patterns instead of glob. I'm going to remove support for the previous settings in functions, so this is a breaking change. Also I just cleaned up the deploy signature since there was already a breaking change

--- appservice/src/deploy/deployZip.ts
+++ appservice/src/deploy/deployZip.ts
@@ -17,7 +17,7 @@ import { deployToStorageAccount } from './deployToStorageAccount';
 import { formatDeployLog } from './formatDeployLog';
 import { waitForDeploymentToComplete } from './waitForDeploymentToComplete';
 
-export async function deployZip(client: SiteClient, fsPath: string, configurationSectionName: string, aspPromise: Promise<AppServicePlan | undefined>): Promise<void> {
+export async function deployZip(client: SiteClient, fsPath: string, aspPromise: Promise<AppServicePlan | undefined>): Promise<void> {
     if (!(await fse.pathExists(fsPath))) {
         throw new Error(localize('pathNotExist', 'Failed to deploy path that does not exist: {0}', fsPath));
     }
@@ -29,7 +29,7 @@ export async function deployZip(client: SiteClient, fsPath: string, configuratio
     } else {
         createdZip = true;
         ext.outputChannel.appendLine(formatDeployLog(client, localize('zipCreate', 'Creating zip package...')));
-        zipFilePath = await getZipFileToDeploy(fsPath, configurationSectionName);
+        zipFilePath = await getZipFileToDeploy(fsPath, client.isFunctionApp);
     }
 
     try {
@@ -56,12 +56,16 @@ export async function deployZip(client: SiteClient, fsPath: string, configuratio
     }
 }
 
-async function getZipFileToDeploy(fsPath: string, configurationSectionName?: string): Promise<string> {
-    if (await FileUtilities.isDirectory(fsPath)) {
-        const zipDeployConfig: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration(configurationSectionName, vscode.Uri.file(fsPath));
-        const globPattern: string | undefined = zipDeployConfig.get<string>('zipGlobPattern');
-        const ignorePattern: string | string[] | undefined = zipDeployConfig.get<string | string[]>('zipIgnorePattern');
-        return FileUtilities.zipDirectory(fsPath, globPattern, ignorePattern);
+async function getZipFileToDeploy(fsPath: string, isFunctionApp: boolean): Promise<string> {
+    if ((await fse.lstat(fsPath)).isDirectory()) {
+        if (isFunctionApp) {
+            return FileUtilities.zipDirectoryGitignore(fsPath, '.funcignore');
+        } else {
+            const zipDeployConfig: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('appService', vscode.Uri.file(fsPath));
+            const globPattern: string | undefined = zipDeployConfig.get<string>('zipGlobPattern');
+            const ignorePattern: string | string[] | undefined = zipDeployConfig.get<string | string[]>('zipIgnorePattern');
+            return FileUtilities.zipDirectoryGlob(fsPath, globPattern, ignorePattern);
+        }
     } else {
         return FileUtilities.zipFile(fsPath);
     }
