37002fd5451dd58aba7087f53f8496e2edcefd55
Stephen Weatherford <stephwe@microsoft.com>
2019-02-20 16:48:32 -0800

    Update test infrastructure

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -9,8 +9,6 @@ const cp = require('child_process');
 function test() {
     const env = process.env;
     env.DEBUGTELEMETRY = 1;
-    env.MOCHA_reporter = 'mocha-junit-reporter';
-    env.MOCHA_FILE = path.join(__dirname, 'test-results.xml');
     env.CODE_EXTENSIONS_PATH = path.join(__dirname, 'test', 'extension');
     return cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
 };
