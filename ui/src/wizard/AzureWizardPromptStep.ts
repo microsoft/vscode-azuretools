4f9881ca8d72a0703047b59e48811225fb354475
Eric Jizba <erijiz@microsoft.com>
2019-03-18 10:22:37 -0700

    Use a tree to keep track of wizard execute steps

--- ui/src/wizard/AzureWizardPromptStep.ts
+++ ui/src/wizard/AzureWizardPromptStep.ts
@@ -4,19 +4,21 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as types from '../../index';
+import { IWizardNode } from './IWizardNode';
 
 export abstract class AzureWizardPromptStep<T> implements types.AzureWizardPromptStep<T> {
+    public hasSubWizard: boolean;
     public numSubPromptSteps: number;
-    public numSubExecuteSteps: number;
+    public wizardNode: IWizardNode<T>;
     public propertiesBeforePrompt: string[];
     public prompted: boolean;
 
     public abstract prompt(wizardContext: T): Promise<types.ISubWizardOptions<T> | void>;
     public abstract shouldPrompt(wizardContext: T): boolean;
 
-    public init(): void {
+    public reset(): void {
+        this.hasSubWizard = false;
         this.numSubPromptSteps = 0;
-        this.numSubExecuteSteps = 0;
         this.prompted = false;
     }
 }
