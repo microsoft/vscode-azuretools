c40bdb7fb691f8629106eee6bf9ea0c0f3cadcfe
Stephen Weatherford <stephwe@microsoft.com>
2019-04-24 12:58:10 -0700

    Fix report an issue URLs

--- ui/src/reportAnIssue.ts
+++ ui/src/reportAnIssue.ts
@@ -6,6 +6,7 @@
 import * as vscode from 'vscode';
 import { IParsedError } from '../index';
 import { getPackageInfo } from "./getPackageInfo";
+import { limitLines, numberOfLines } from './utils/limitLines';
 import { openUrl } from './utils/openUrl';
 
 /**
@@ -14,7 +15,14 @@ import { openUrl } from './utils/openUrl';
 export async function reportAnIssue(actionId: string, parsedError: IParsedError): Promise<void> {
     const { extensionName, extensionVersion, bugsUrl } = getPackageInfo();
 
-    let body: string = `
+    // Some browsers don't have very long URLs
+    // tslint:disable-next-line: typedef
+    const maxUrlLength = 2000;
+
+    // tslint:disable-next-line: strict-boolean-expressions
+    const stack: string = parsedError.stack || '';
+
+    const body: string = `
 <!-- IMPORTANT: Please be sure to remove any private information before submitting. -->
 
 Repro steps:
@@ -33,22 +41,30 @@ Product: ${vscode.env.appName}
 Product Version: ${vscode.version}
 Language: ${vscode.env.language}`;
 
-    if (parsedError.stack) {
-        body = body.concat(`
+    // Try repeatedly with fewer lines of stack until we have a URL of a reasonable length
+    // tslint:disable-next-line: no-increment-decrement
+    for (let stackLines: number = numberOfLines(stack); stackLines >= 0; --stackLines) {
+        let bodyWithStack: string = body;
+        if (stack) {
+            bodyWithStack = bodyWithStack.concat(`
 
-<details>
+            <details>
 <summary>Call Stack</summary>
 
 \`\`\`
-${parsedError.stack}
+${limitLines(stack, stackLines)}
 \`\`\`
 
 </details>`);
+
+            const baseUrl: string = bugsUrl || `https://github.com/Microsoft/${extensionName}/issues`;
+            const url: string = `${baseUrl}/new?body=${encodeURIComponent(bodyWithStack)}`;
+
+            if (url.length < maxUrlLength || stackLines === 0) {
+                // tslint:disable-next-line:no-floating-promises
+                await openUrl(url);
+                return;
+            }
+        }
     }
-
-    const baseUrl: string = bugsUrl || `https://github.com/Microsoft/${extensionName}/issues`;
-    const url: string = `${baseUrl}/new?body=${encodeURIComponent(body)}`;
-
-    // tslint:disable-next-line:no-floating-promises
-    await openUrl(url);
 }
--- ui/src/utils/limitLines.ts
+++ ui/src/utils/limitLines.ts
@@ -10,3 +10,8 @@ export function limitLines(s: string, n: number): string {
     const match: RegExpMatchArray | null = s.match(new RegExp(`((\\r\\n|\\n)?.*$){0,${n}}`, 'm'));
     return match ? match[0] : '';
 }
+
+export function numberOfLines(s: string): number {
+    const match: RegExpMatchArray | null = s.match(/(\r\n|\n)/g);
+    return match ? match.length + 1 : 1;
+}
