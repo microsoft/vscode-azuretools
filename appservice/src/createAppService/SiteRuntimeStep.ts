4223a6e97d026f24e59d8c2cf4aff0c40311a0cf
William Lorey <wilorey@microsoft.com>
2019-10-08 16:39:00 -0700

    Render Linux runtimes properly when creating web app (#597)
    * Add display name
    
    * Filter out Node 6 LTS
    
    * Update tests

--- appservice/src/createAppService/SiteRuntimeStep.ts
+++ appservice/src/createAppService/SiteRuntimeStep.ts
@@ -68,16 +68,19 @@ export function convertStacksToPicks(stacks: ApplicationStack[], recommendedRunt
         // convert each "majorVersion" to an object with all the info we need
         // tslint:disable-next-line: strict-boolean-expressions
         .map(stack => (stack.majorVersions || []).map(mv => {
+            const stackDisplay: string = nonNullProp(stack, 'display');
             return {
                 runtimeVersion: nonNullProp(mv, 'runtimeVersion'),
-                displayVersion: nonNullProp(mv, 'displayVersion'),
-                stackDisplay: nonNullProp(stack, 'display')
+                stackDisplay: stackDisplay,
+                // include stack display in the display name only if it doesn't include a version number
+                // this simplifies names such as 'Java 8 Tomcat 8.5' to just 'Tomcat 8.5'
+                displayName: /[0-9]/.test(stackDisplay) ? nonNullProp(mv, 'displayVersion') : `${stackDisplay} ${nonNullProp(mv, 'displayVersion')}`
             };
         }))
         // flatten array
         .reduce((acc, val) => acc.concat(val))
-        // filter out Node 4.x and 6.x as they are EOL
-        .filter(mv => !/node\|(4|6)\./i.test(mv.runtimeVersion))
+        // filter out Node 4.x, 6.x, and 6 LTS as they are EOL
+        .filter(mv => !/node\|(4|6)[\.-]/i.test(mv.runtimeVersion))
         // filter out .NET Core 1.x and 2.0 as they are EOL
         .filter(mv => !/dotnetcore\|(1\.|2\.0)/i.test(mv.runtimeVersion))
         // sort
@@ -95,8 +98,8 @@ export function convertStacksToPicks(stacks: ApplicationStack[], recommendedRunt
                 return bInfo.minor - aInfo.minor;
             }
 
-            if (a.displayVersion !== b.displayVersion) {
-                return a.displayVersion.localeCompare(b.displayVersion);
+            if (a.displayName !== b.displayName) {
+                return a.displayName.localeCompare(b.displayName);
             } else {
                 return a.stackDisplay.localeCompare(b.stackDisplay);
             }
@@ -105,7 +108,7 @@ export function convertStacksToPicks(stacks: ApplicationStack[], recommendedRunt
         .map(mv => {
             return {
                 id: mv.runtimeVersion,
-                label: mv.displayVersion,
+                label: mv.displayName,
                 data: mv.runtimeVersion,
                 // include stack as description if it has a version
                 // tslint:disable-next-line: strict-boolean-expressions
