7f91634de28b4f237141c3eaf92adaec9c92b62a
Stephen Weatherford <stephwe@microsoft.com>
2019-11-15 18:52:30 -0800

    Support actionContext.errorHandling.issueProperties and pasting long issues to clipboard

--- ui/test/reportAnIssue.test.ts
+++ ui/test/reportAnIssue.test.ts
@@ -6,72 +6,14 @@
 // tslint:disable: max-func-body-length
 
 import * as assert from 'assert';
+import { env } from 'vscode';
 import { IParsedError } from '..';
 import { getReportAnIssueLink, maxUrlLength } from '../src/reportAnIssue';
 
 suite('getReportAnIssueLink', () => {
-    test('message too long, no stack', () => {
-        const message: string = 'x'.repeat(maxUrlLength);
-        const pe: IParsedError = {
-            errorType: 'error Type',
-            isUserCancelledError: false,
-            message: message,
-            stack: undefined
-        };
+    const pasteIntoWindowBody: string = "https://github.com/Microsoft/azureextensionui/issues/new?body=The%20issue%20text%20was%20copied%20to%20the%20clipboard.%20%20Please%20paste%20it%20into%20this%20window.";
 
-        const link: string = getReportAnIssueLink('actionId', pe);
-        assert(link.length <= maxUrlLength);
-        assert(linkIncludes(link, '...'));
-    });
-
-    test('message too long, with stack', () => {
-        const message: string = '#?x '.repeat(maxUrlLength);
-        const pe: IParsedError = {
-            errorType: 'error Type',
-            isUserCancelledError: false,
-            message: message,
-            stack: createStack(10)
-        };
-
-        const link: string = getReportAnIssueLink('actionId', pe);
-        assert(link.length <= maxUrlLength);
-        assert(!linkIncludes(link, 'Stack line'));
-        assert(linkIncludes(link, '...'));
-    });
-
-    test('no stack', () => {
-        const message: string = 'This is a "message"?';
-        const pe: IParsedError = {
-            errorType: 'error Type',
-            isUserCancelledError: false,
-            message: message,
-            stack: undefined
-        };
-
-        const link: string = getReportAnIssueLink('actionId', pe);
-        assert(link.length <= maxUrlLength);
-        assert(linkIncludes(link, message));
-        assert(!linkIncludes(link, 'Call Stack'));
-        assert(!linkIncludes(link, '...'));
-    });
-
-    test('empty stack', () => {
-        const message: string = 'This is a "message"?';
-        const pe: IParsedError = {
-            errorType: 'error Type',
-            isUserCancelledError: false,
-            message: message,
-            stack: ''
-        };
-
-        const link: string = getReportAnIssueLink('actionId', pe);
-        assert(link.length <= maxUrlLength);
-        assert(linkIncludes(link, message));
-        assert(!linkIncludes(link, 'Call Stack'));
-        assert(!linkIncludes(link, '...'));
-    });
-
-    test('Short stack', () => {
+    test('Short stack, entire body should be in link', async () => {
         const message: string = 'This is a "message"?';
         const pe: IParsedError = {
             errorType: 'error Type',
@@ -80,33 +22,18 @@ suite('getReportAnIssueLink', () => {
             stack: createStack(1)
         };
 
-        const link: string = getReportAnIssueLink('actionId', pe);
+        const link: string = await getReportAnIssueLink('actionId', pe, {});
+
         assert(link.length <= maxUrlLength);
+        assert.notEqual(link, pasteIntoWindowBody);
+        assert(linkIncludes(link, "Action: actionId"));
         assert(linkIncludes(link, message));
         assert(linkIncludes(link, 'Call Stack'));
         assert(linkIncludes(link, 'Stack line #1'));
         assert(!linkIncludes(link, '...'));
     });
 
-    test('Short stack 2', () => {
-        const message: string = 'This is a "message"?';
-        const pe: IParsedError = {
-            errorType: 'error Type',
-            isUserCancelledError: false,
-            message: message,
-            stack: createStack(2)
-        };
-
-        const link: string = getReportAnIssueLink('actionId', pe);
-        assert(link.length <= maxUrlLength);
-        assert(linkIncludes(link, message));
-        assert(linkIncludes(link, 'Call Stack'));
-        assert(linkIncludes(link, 'Stack line #1'));
-        assert(linkIncludes(link, 'Stack line #2'));
-        assert(!linkIncludes(link, '...'));
-    });
-
-    test('Long stack', () => {
+    test('Long stack - should copy body to clipboard', async () => {
         const message: string = 'This is a "message"?';
         const pe: IParsedError = {
             errorType: 'error Type',
@@ -115,12 +42,86 @@ suite('getReportAnIssueLink', () => {
             stack: createStack(maxUrlLength) // definitely too long :-)
         };
 
-        const link: string = getReportAnIssueLink('actionId', pe);
+        const link: string = await getReportAnIssueLink('actionId', pe, {}/*asdf*/);
+
         assert(link.length <= maxUrlLength);
-        assert(linkIncludes(link, message));
-        assert(linkIncludes(link, 'Call Stack'));
-        assert(linkIncludes(link, 'Stack line #1'));
-        assert(!linkIncludes(link, '...'));
+        assert.equal(link, pasteIntoWindowBody);
+
+        const body: string = await env.clipboard.readText();
+        assert(body.includes("Action: actionId"));
+        assert(body.includes(message));
+        assert(body.includes('Call Stack'));
+        assert(body.includes('Stack line #1'));
+    });
+
+    test('message long - should copy body to clipboard', async () => {
+        const message: string = 'x'.repeat(maxUrlLength);
+        const pe: IParsedError = {
+            errorType: 'error Type',
+            isUserCancelledError: false,
+            message: message,
+            stack: undefined
+        };
+
+        const link: string = await getReportAnIssueLink('actionId', pe, {});
+
+        assert.equal(link, pasteIntoWindowBody);
+
+        const body: string = await env.clipboard.readText();
+        assert(body.includes("Action: actionId"));
+        assert(body.includes(message));
+    });
+
+    suite("issueProperties", () => {
+        test('single property', async () => {
+            const message: string = "This is my message";
+            const pe: IParsedError = {
+                errorType: 'error Type',
+                isUserCancelledError: false,
+                message: message,
+                stack: undefined
+            };
+
+            const link: string = await getReportAnIssueLink('actionId', pe, { property1: "Property #1" });
+
+            assert(linkIncludes(link, "property1"));
+            assert(linkIncludes(link, "Property #1"));
+        });
+
+        test('multiple properties', async () => {
+            const message: string = "This is my message";
+            const pe: IParsedError = {
+                errorType: 'error Type',
+                isUserCancelledError: false,
+                message: message,
+                stack: undefined
+            };
+
+            const link: string = await getReportAnIssueLink('actionId', pe, { property1: "Property #1", property2: "Property #2" });
+
+            assert(linkIncludes(link, "property1"));
+            assert(linkIncludes(link, "Property #1"));
+            assert(linkIncludes(link, "property2"));
+            assert(linkIncludes(link, "Property #2"));
+        });
+
+        test('multiple properties and stack', async () => {
+            const message: string = "This is my message";
+            const pe: IParsedError = {
+                errorType: 'error Type',
+                isUserCancelledError: false,
+                message: message,
+                stack: createStack(1)
+            };
+
+            const link: string = await getReportAnIssueLink('actionId', pe, { property1: "Property #1", property2: "Property #2" });
+
+            assert(linkIncludes(link, "property1"));
+            assert(linkIncludes(link, "Property #1"));
+            assert(linkIncludes(link, "property2"));
+            assert(linkIncludes(link, "Property #2"));
+            assert(linkIncludes(link, "Stack line #1"));
+        });
     });
 });
 
