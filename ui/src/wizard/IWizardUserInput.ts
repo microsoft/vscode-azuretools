fbff309cab101b7e5297a7b7f7b826499a76f8ed
Eric Jizba <erijiz@microsoft.com>
2020-02-18 11:09:08 -0800

    Fix wizard timing issues

---
+++ ui/src/wizard/IWizardUserInput.ts
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { InputBoxOptions, QuickPickItem, QuickPickOptions } from "vscode";
+
+export interface IWizardUserInput extends IRootUserInput {
+    isPrompting: boolean;
+}
+
+export interface IRootUserInput {
+    showQuickPick<T extends QuickPickItem>(picks: T[] | Thenable<T[]>, options: QuickPickOptions): Thenable<T>;
+    showInputBox(options: InputBoxOptions): Thenable<string | undefined>;
+}
