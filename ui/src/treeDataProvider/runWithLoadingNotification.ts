10a7dfe22258ee1332766e10f05817df3d923c15
William Lorey <wilorey@microsoft.com>
2020-05-18 15:06:48 -0700

    Only show one long running notification at a time in runWithLoadingNotification

--- ui/src/treeDataProvider/runWithLoadingNotification.ts
+++ ui/src/treeDataProvider/runWithLoadingNotification.ts
@@ -3,20 +3,31 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { CancellationToken, ProgressLocation, window } from 'vscode';
+import { CancellationToken, EventEmitter, ProgressLocation, window } from 'vscode';
 import * as types from '../../index';
 import { localize } from '../localize';
 
-export async function runWithLoadingNotification<T>(context: types.ILoadingTreeContext, callback: (cancellationToken: CancellationToken) => Promise<T>): Promise<T> {
-    return await window.withProgress({ location: ProgressLocation.Notification, cancellable: true }, async (progress, cancellationToken) => {
-        const message: string = context.loadingMessage || localize('loadingAll', 'Loading resources...');
-        const messageDelay: number = context.loadingMessageDelay !== undefined ? context.loadingMessageDelay : 2;
-        const timer: NodeJS.Timer = setTimeout(() => progress.report({ message }), messageDelay * 1000);
+let notificationActive: boolean = false;
 
-        try {
-            return await callback(cancellationToken);
-        } finally {
-            clearTimeout(timer);
-        }
-    });
+export async function runWithLoadingNotification<T>(context: types.ILoadingTreeContext, callback: (cancellationToken: CancellationToken) => Promise<T>): Promise<T> {
+    if (notificationActive) {
+        return await callback({
+            isCancellationRequested: false,
+            onCancellationRequested: (new EventEmitter()).event
+        });
+    } else {
+        return await window.withProgress({ location: ProgressLocation.Notification, cancellable: true }, async (progress, cancellationToken) => {
+            notificationActive = true;
+            const message: string = context.loadingMessage || localize('loadingAll', 'Loading resources...');
+            const messageDelay: number = context.loadingMessageDelay !== undefined ? context.loadingMessageDelay : 2;
+            const timer: NodeJS.Timer = setTimeout(() => progress.report({ message }), messageDelay * 1000);
+
+            try {
+                return await callback(cancellationToken);
+            } finally {
+                clearTimeout(timer);
+                notificationActive = false;
+            }
+        });
+    }
 }
