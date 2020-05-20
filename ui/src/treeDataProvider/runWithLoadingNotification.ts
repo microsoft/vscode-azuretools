7f5c343667ad02475e805cf57a7894dbda3692c0
William Lorey <wilorey@microsoft.com>
2020-05-19 18:50:02 -0700

    Revert runWithLoadingNotification.ts

--- ui/src/treeDataProvider/runWithLoadingNotification.ts
+++ ui/src/treeDataProvider/runWithLoadingNotification.ts
@@ -7,24 +7,16 @@ import { CancellationToken, ProgressLocation, window } from 'vscode';
 import * as types from '../../index';
 import { localize } from '../localize';
 
-let activeCancellationToken: CancellationToken | undefined;
-
 export async function runWithLoadingNotification<T>(context: types.ILoadingTreeContext, callback: (cancellationToken: CancellationToken) => Promise<T>): Promise<T> {
-    if (!!activeCancellationToken) {
-        return await callback(activeCancellationToken);
-    } else {
-        return await window.withProgress({ location: ProgressLocation.Notification, cancellable: true }, async (progress, cancellationToken) => {
-            const message: string = context.loadingMessage || localize('loadingAll', 'Loading resources...');
-            const messageDelay: number = context.loadingMessageDelay !== undefined ? context.loadingMessageDelay : 2;
-            const timer: NodeJS.Timer = setTimeout(() => progress.report({ message }), messageDelay * 1000);
-            activeCancellationToken = cancellationToken;
+    return await window.withProgress({ location: ProgressLocation.Notification, cancellable: true }, async (progress, cancellationToken) => {
+        const message: string = context.loadingMessage || localize('loadingAll', 'Loading resources...');
+        const messageDelay: number = context.loadingMessageDelay !== undefined ? context.loadingMessageDelay : 2;
+        const timer: NodeJS.Timer = setTimeout(() => progress.report({ message }), messageDelay * 1000);
 
-            try {
-                return await callback(activeCancellationToken);
-            } finally {
-                clearTimeout(timer);
-                activeCancellationToken = undefined;
-            }
-        });
-    }
+        try {
+            return await callback(cancellationToken);
+        } finally {
+            clearTimeout(timer);
+        }
+    });
 }
