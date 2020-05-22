bd42b0822b1e3485ea0251efe0e671969c067a10
alexweininger <alex.weininger@live.com>
2020-05-21 17:46:03 -0700

    Basic client needed for app settings

---
+++ appservice/src/IAppSettingsClient.ts
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { StringDictionary } from 'azure-arm-website/lib/models';
+
+export interface IAppSettingsClient {
+
+    isLinux: boolean;
+
+    listApplicationSettings(): Promise<StringDictionary>;
+
+    updateApplicationSettings(appSettings: StringDictionary): Promise<StringDictionary>;
+}
