575882417a775819abfe769183accc1b8879b8aa
Jeffrey Chen <t-jefche@microsoft.com>
2020-07-07 17:26:10 -0400

    Set 'acceptLanguage' option to VS Code language when creating Azure client

--- ui/src/createAzureClient.ts
+++ ui/src/createAzureClient.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as vscode from 'vscode';
 import { ServiceClientCredentials } from "ms-rest";
 import { AzureEnvironment, AzureServiceClientOptions } from "ms-rest-azure";
 import { IAddUserAgent } from "../index";
@@ -11,7 +12,7 @@ import { addExtensionUserAgent } from "./extensionUserAgent";
 export function createAzureClient<T extends IAddUserAgent>(
     clientInfo: { credentials: ServiceClientCredentials; subscriptionId: string; environment: AzureEnvironment; },
     clientType: new (credentials: ServiceClientCredentials, subscriptionId: string, baseUri?: string, options?: AzureServiceClientOptions) => T): T {
-    const client: T = new clientType(clientInfo.credentials, clientInfo.subscriptionId, clientInfo.environment.resourceManagerEndpointUrl);
+    const client: T = new clientType(clientInfo.credentials, clientInfo.subscriptionId, clientInfo.environment.resourceManagerEndpointUrl, { acceptLanguage: vscode.env.language });
     addExtensionUserAgent(client);
     return client;
 }
