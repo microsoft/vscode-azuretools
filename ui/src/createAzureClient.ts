4bd2e825755ba41e7a12bfcae935488d1d447a8e
Jeffrey <t-jefche@microsoft.com>
2020-07-08 14:02:48 -0400

    Set 'acceptLanguage' option to VS Code language when creating Azure client (#763)

--- ui/src/createAzureClient.ts
+++ ui/src/createAzureClient.ts
@@ -5,13 +5,14 @@
 
 import { ServiceClientCredentials } from "ms-rest";
 import { AzureEnvironment, AzureServiceClientOptions } from "ms-rest-azure";
+import * as vscode from "vscode";
 import { IAddUserAgent } from "../index";
 import { addExtensionUserAgent } from "./extensionUserAgent";
 
 export function createAzureClient<T extends IAddUserAgent>(
     clientInfo: { credentials: ServiceClientCredentials; subscriptionId: string; environment: AzureEnvironment; },
     clientType: new (credentials: ServiceClientCredentials, subscriptionId: string, baseUri?: string, options?: AzureServiceClientOptions) => T): T {
-    const client: T = new clientType(clientInfo.credentials, clientInfo.subscriptionId, clientInfo.environment.resourceManagerEndpointUrl);
+    const client: T = new clientType(clientInfo.credentials, clientInfo.subscriptionId, clientInfo.environment.resourceManagerEndpointUrl, { acceptLanguage: vscode.env.language });
     addExtensionUserAgent(client);
     return client;
 }
@@ -19,7 +20,7 @@ export function createAzureClient<T extends IAddUserAgent>(
 export function createAzureSubscriptionClient<T extends IAddUserAgent>(
     clientInfo: { credentials: ServiceClientCredentials; environment: AzureEnvironment; },
     clientType: new (credentials: ServiceClientCredentials, baseUri?: string, options?: AzureServiceClientOptions) => T): T {
-    const client: T = new clientType(clientInfo.credentials, clientInfo.environment.resourceManagerEndpointUrl);
+    const client: T = new clientType(clientInfo.credentials, clientInfo.environment.resourceManagerEndpointUrl, { acceptLanguage: vscode.env.language });
     addExtensionUserAgent(client);
     return client;
 }
