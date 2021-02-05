9bff33c5bee3fc8581abe0c3aef6cf14acd7ffc3
Eric Jizba <erijiz@microsoft.com>
2021-02-05 13:16:41 -0800

    Better handling of json in requests

--- ui/src/createAzureClient.ts
+++ ui/src/createAzureClient.ts
@@ -4,10 +4,11 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Environment } from '@azure/ms-rest-azure-env';
-import { ServiceClient, ServiceClientCredentials } from '@azure/ms-rest-js';
+import { BaseRequestPolicy, HttpOperationResponse, RequestPolicy, RequestPolicyFactory, RequestPolicyOptions, ServiceClient, ServiceClientCredentials, WebResourceLike } from '@azure/ms-rest-js';
 import * as vscode from "vscode";
 import * as types from '../index';
 import { appendExtensionUserAgent } from "./extensionUserAgent";
+import { parseJson, removeBom } from './utils/parseJson';
 
 export function createAzureClient<T>(
     clientInfo: { credentials: ServiceClientCredentials; subscriptionId: string; environment: Environment; },
@@ -15,7 +16,8 @@ export function createAzureClient<T>(
     return new clientType(clientInfo.credentials, clientInfo.subscriptionId, {
         acceptLanguage: vscode.env.language,
         baseUri: clientInfo.environment.resourceManagerEndpointUrl,
-        userAgent: appendExtensionUserAgent
+        userAgent: appendExtensionUserAgent,
+        requestPolicyFactories: addAzExtFactories
     });
 }
 
@@ -25,7 +27,8 @@ export function createAzureSubscriptionClient<T>(
     return new clientType(clientInfo.credentials, {
         acceptLanguage: vscode.env.language,
         baseUri: clientInfo.environment.resourceManagerEndpointUrl,
-        userAgent: appendExtensionUserAgent
+        userAgent: appendExtensionUserAgent,
+        requestPolicyFactories: addAzExtFactories
     });
 }
 
@@ -43,6 +46,63 @@ export async function createGenericClient(clientInfo?: ServiceClientCredentials 
 
     return new gsc.GenericServiceClient(credentials, <types.IMinimumServiceClientOptions>{
         baseUri,
-        userAgent: appendExtensionUserAgent
+        userAgent: appendExtensionUserAgent,
+        requestPolicyFactories: addAzExtFactories
     });
+}
+
+function addAzExtFactories(defaultFactories: RequestPolicyFactory[]): RequestPolicyFactory[] {
+    // NOTE: Factories at the end of the array are executed first, and we want these to happen before the deserialization factory
+    defaultFactories.push(
+        {
+            create: (nextPolicy, options) => new RemoveBOMFactory(nextPolicy, options)
+        },
+        {
+            create: (nextPolicy, options) => new MissingContentTypeFactory(nextPolicy, options)
+        }
+    );
+    return defaultFactories;
+}
+
+const contentTypeName: string = 'Content-Type';
+
+/**
+ * Removes the BOM character if it exists in bodyAsText for a json response, to prevent a parse error
+ */
+class RemoveBOMFactory extends BaseRequestPolicy {
+    constructor(nextPolicy: RequestPolicy, requestPolicyOptions: RequestPolicyOptions) {
+        super(nextPolicy, requestPolicyOptions);
+    }
+
+    public async sendRequest(request: WebResourceLike): Promise<HttpOperationResponse> {
+        const response: HttpOperationResponse = await this._nextPolicy.sendRequest(request);
+        const contentType: string | undefined = response.headers.get(contentTypeName);
+        if (contentType && /json/i.test(contentType) && response.bodyAsText) {
+            response.bodyAsText = removeBom(response.bodyAsText);
+        }
+        return response;
+    }
+}
+
+/**
+ * The Azure SDK will assume "JSON" if no content-type is specified, which can cause false-positive parse errors.
+ * This will be a little smarter and try to detect if it's json or generic data
+ */
+class MissingContentTypeFactory extends BaseRequestPolicy {
+    constructor(nextPolicy: RequestPolicy, requestPolicyOptions: RequestPolicyOptions) {
+        super(nextPolicy, requestPolicyOptions);
+    }
+
+    public async sendRequest(request: WebResourceLike): Promise<HttpOperationResponse> {
+        const response: HttpOperationResponse = await this._nextPolicy.sendRequest(request);
+        if (!response.headers.get(contentTypeName) && response.bodyAsText) {
+            try {
+                parseJson(response.bodyAsText);
+                response.headers.set(contentTypeName, 'application/json');
+            } catch {
+                response.headers.set(contentTypeName, 'application/octet-stream');
+            }
+        }
+        return response;
+    }
 }
