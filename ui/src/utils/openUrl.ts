ba7a784e2606f2f20c29a13a03a38dbdb5dde78e
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-03-25 12:55:24 -0400

    Use VSCode API for URL opening

--- ui/src/utils/openUrl.ts
+++ ui/src/utils/openUrl.ts
@@ -3,13 +3,8 @@
  *  Licensed under the MIT License. See License.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// tslint:disable-next-line:no-require-imports
-import opn = require("opn");
+import * as vscode from 'vscode';
 
 export async function openUrl(url: string): Promise<void> {
-    // Using this functionality is blocked by https://github.com/Microsoft/vscode/issues/25852:
-    // await vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(url));
-
-    // tslint:disable-next-line: no-unsafe-any
-    opn(url);
+    await vscode.env.openExternal(vscode.Uri.parse(url));
 }
