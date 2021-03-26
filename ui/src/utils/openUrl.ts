fa291663c1a0ad4379d3aa56c9ac43a1f6c26355
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2021-03-26 15:14:23 -0400

    Eric's feedback

--- ui/src/utils/openUrl.ts
+++ ui/src/utils/openUrl.ts
@@ -3,8 +3,13 @@
  *  Licensed under the MIT License. See License.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as vscode from 'vscode';
+// tslint:disable-next-line:no-require-imports
+import open = require("open");
 
 export async function openUrl(url: string): Promise<void> {
-    await vscode.env.openExternal(vscode.Uri.parse(url));
+    // Using this functionality is blocked by https://github.com/Microsoft/vscode/issues/25852:
+    // await vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(url));
+
+    // tslint:disable-next-line: no-unsafe-any
+    open(url);
 }
