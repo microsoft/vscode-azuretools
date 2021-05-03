b7902f6d57c747bb30803239fffd16372491e2f2
nepothar <nepothar@microsoft.com>
2021-05-03 16:10:11 -0700

    updated AppInsightsCreateStep to fix AuthorizationFailed errors

--- appservice/src/createAppService/AppInsightsCreateStep.ts
+++ appservice/src/createAppService/AppInsightsCreateStep.ts
@@ -36,7 +36,6 @@ export class AppInsightsCreateStep extends AzureWizardExecuteStep<IAppServiceWiz
                 ext.outputChannel.appendLog(localize('existingNewAppInsights', 'Using existing Application Insights resource "{0}".', aiName));
             } catch (error) {
                 const pError: IParsedError = parseError(error);
-                // Only expecting a resource not found error if this is a new component
                 if (pError.errorType === 'ResourceNotFound') {
                     const creatingNewAppInsights: string = localize('creatingNewAppInsightsInsights', 'Creating Application Insights resource "{0}"...', aiName);
                     ext.outputChannel.appendLog(creatingNewAppInsights);
@@ -45,6 +44,9 @@ export class AppInsightsCreateStep extends AzureWizardExecuteStep<IAppServiceWiz
                     wizardContext.appInsightsComponent = await client.components.createOrUpdate(rgName, aiName, { kind: 'web', applicationType: 'web', location: appInsightsLocation });
                     const createdNewAppInsights: string = localize('createdNewAppInsights', 'Successfully created Application Insights resource "{0}".', aiName);
                     ext.outputChannel.appendLog(createdNewAppInsights);
+                } else if (wizardContext.suppress403Handling || pError.errorType !== '403') {
+                    const createAppInsightsNotAvailable: string = localize('appInsightsNotAvailable', 'Skipping creating an Application Insights because the user doesn\'t have the right permissions.');
+                    ext.outputChannel.appendLog(createAppInsightsNotAvailable);
                 } else {
                     throw error;
                 }
