c4aba278c04834f85d02b93434b9879c87b36218
Nathan <naturins@microsoft.com>
2021-05-06 17:55:21 -0700

    Automatically pick rg for sandbox subscriptions (#889)

--- ui/src/wizard/ResourceGroupCreateStep.ts
+++ ui/src/wizard/ResourceGroupCreateStep.ts
@@ -38,6 +38,15 @@ export class ResourceGroupCreateStep<T extends types.IResourceGroupWizardContext
             if (wizardContext.suppress403Handling || parseError(error).errorType !== '403') {
                 throw error;
             } else {
+                // if we suspect that this is a Concierge account, only pick the rg if it begins with "learn" and there is only 1
+                if (/concierge/i.test(wizardContext.subscriptionDisplayName)) {
+                    const rgs = await resourceClient.resourceGroups.list();
+                    if (rgs.length === 1 && rgs[0].name && /^learn/i.test(rgs[0].name)) {
+                        wizardContext.resourceGroup = rgs[0];
+                        return undefined;
+                    }
+                }
+
                 const message: string = localize('rgForbidden', 'You do not have permission to create a resource group in subscription "{0}".', wizardContext.subscriptionDisplayName);
                 const selectExisting: MessageItem = { title: localize('selectExisting', 'Select Existing') };
                 wizardContext.telemetry.properties.cancelStep = 'RgNoPermissions';
