16c8b37fd3e75fbfc7c63bcf67e5a889577bf8a9
nturinski <naturins@microsoft.com>
2021-05-18 12:02:50 -0700

    Linting

--- appservice/src/deploy/runWithZipStream.ts
+++ appservice/src/deploy/runWithZipStream.ts
@@ -34,12 +34,14 @@ export async function runWithZipStream(context: IActionContext, fsPath: string, 
         });
     } else {
         ext.outputChannel.appendLog(localize('zipCreate', 'Creating zip package...'), { resourceName: client.fullName });
-        const zipFile: yazl.ZipFile & { outputStreamCursor?: number } = new yazl.ZipFile();
+        const zipFile: yazl.ZipFile = new yazl.ZipFile();
         let filesToZip: string[] = [];
         let sizeOfZipFile: number = 0;
 
         zipFile.outputStream.on('data', (chunk) => {
-            sizeOfZipFile += chunk.length
+            if (Array.isArray(chunk)) {
+                sizeOfZipFile += chunk.length
+            }
         });
 
         zipFile.outputStream.on('finish', () => onFileSize(sizeOfZipFile));
