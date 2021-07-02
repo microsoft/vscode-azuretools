3cac3f9da1bd85f07144ec24e8e9e5889902f511
Eric Jizba <erijiz@microsoft.com>
2021-07-01 17:04:44 -0700

    Add TestActionContext to dev package (#947)

---
+++ dev/src/TestActionContext.ts
@@ -0,0 +1,27 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import * as types from '../index';
+import { TestUserInput } from './TestUserInput';
+
+export function createTestActionContext(): types.TestActionContext {
+    return { telemetry: { properties: {}, measurements: {} }, errorHandling: { issueProperties: {} }, valuesToMask: [], ui: new TestUserInput(vscode) };
+}
+
+/**
+ * Similar to `createTestActionContext` but with some extra logging
+ */
+export async function runWithTestActionContext(callbackId: string, callback: (context: types.TestActionContext) => Promise<void>): Promise<void> {
+    const context = createTestActionContext();
+    const start: number = Date.now();
+    try {
+        await callback(context);
+    } finally {
+        const end: number = Date.now();
+        context.telemetry.measurements.duration = (end - start) / 1000;
+        console.log(`** TELEMETRY(${callbackId}) properties=${JSON.stringify(context.telemetry.properties)}, measurements=${JSON.stringify(context.telemetry.measurements)}`);
+    }
+}
