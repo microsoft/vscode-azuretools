ea8288002624d38f125c8c08bfcde226fc86a1ab
William Lorey <wilorey@microsoft.com>
2021-08-09 13:14:05 -0700

    Add test config & files to make it a for-real package"

---
+++ eslint-config-azuretools/test.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "rules": {
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
+        "@typescript-eslint/require-await": "off",
+        "no-dupe-else-if": "off",
+        "no-empty": "off",
+        "no-restricted-imports": ["error", { "patterns": ["**/src/"] }],
+    }
+}
