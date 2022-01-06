28d218c20c5b464a28eef405a81cfb8202e81d7b
William Lorey <wilorey@microsoft.com>
2022-01-06 13:03:59 -0800

    Remove `open` in favor of `vscode.env.openExternal`

--- ui/src/utils/openUrl.ts
+++ ui/src/utils/openUrl.ts
@@ -3,11 +3,8 @@
  *  Licensed under the MIT License. See License.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import open = require("open");
+import { env, Uri } from 'vscode';
 
 export async function openUrl(url: string): Promise<void> {
-    // Using this functionality is blocked by https://github.com/Microsoft/vscode/issues/85930
-    // await vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(url));
-
-    await open(url);
+    await env.openExternal(Uri.parse(url));
 }
