1dc822a20f3068242076995cff563ef30a8c61f6
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-02-10 12:58:21 -0500

    Direct dependency on `@azure/ms-rest-nodeauth`

--- azure/src/azure-account.api.d.ts
+++ azure/src/azure-account.api.d.ts
@@ -6,42 +6,42 @@
 import type { SubscriptionModels } from '@azure/arm-resources-subscriptions';
 import { TokenCredential } from '@azure/core-auth';
 import { Environment } from '@azure/ms-rest-azure-env';
-import { TokenCredentialsBase } from '@azure/ms-rest-nodeauth';
+import type { TokenCredentialsBase } from '@azure/ms-rest-nodeauth';
 import { ReadStream } from 'fs';
 import { CancellationToken, Event, Progress, Terminal } from 'vscode';
 
 export type AzureLoginStatus = 'Initializing' | 'LoggingIn' | 'LoggedIn' | 'LoggedOut';
 
 export interface AzureAccountExtensionApi {
-	readonly apiVersion: string;
-	readonly status: AzureLoginStatus;
-	readonly filters: AzureResourceFilter[];
-	readonly sessions: AzureSession[];
-	readonly subscriptions: AzureSubscription[];
-	readonly onStatusChanged: Event<AzureLoginStatus>;
-	readonly onFiltersChanged: Event<void>;
-	readonly onSessionsChanged: Event<void>;
-	readonly onSubscriptionsChanged: Event<void>;
-	readonly waitForFilters: () => Promise<boolean>;
-	readonly waitForLogin: () => Promise<boolean>;
-	readonly waitForSubscriptions: () => Promise<boolean>;
-	createCloudShell(os: 'Linux' | 'Windows'): CloudShell;
+    readonly apiVersion: string;
+    readonly status: AzureLoginStatus;
+    readonly filters: AzureResourceFilter[];
+    readonly sessions: AzureSession[];
+    readonly subscriptions: AzureSubscription[];
+    readonly onStatusChanged: Event<AzureLoginStatus>;
+    readonly onFiltersChanged: Event<void>;
+    readonly onSessionsChanged: Event<void>;
+    readonly onSubscriptionsChanged: Event<void>;
+    readonly waitForFilters: () => Promise<boolean>;
+    readonly waitForLogin: () => Promise<boolean>;
+    readonly waitForSubscriptions: () => Promise<boolean>;
+    createCloudShell(os: 'Linux' | 'Windows'): CloudShell;
 }
 
 export interface AzureSession {
-	readonly environment: Environment;
-	readonly userId: string;
-	readonly tenantId: string;
+    readonly environment: Environment;
+    readonly userId: string;
+    readonly tenantId: string;
 
-	/**
-	 * The credentials object for azure-sdk-for-js modules https://github.com/azure/azure-sdk-for-js
-	 */
-	readonly credentials2: TokenCredentialsBase & TokenCredential;
+    /**
+     * The credentials object for azure-sdk-for-js modules https://github.com/azure/azure-sdk-for-js
+     */
+    readonly credentials2: TokenCredentialsBase & TokenCredential;
 }
 
 export interface AzureSubscription {
-	readonly session: AzureSession;
-	readonly subscription: SubscriptionModels.Subscription;
+    readonly session: AzureSession;
+    readonly subscription: SubscriptionModels.Subscription;
 }
 
 export type AzureResourceFilter = AzureSubscription;
@@ -49,16 +49,16 @@ export type AzureResourceFilter = AzureSubscription;
 export type CloudShellStatus = 'Connecting' | 'Connected' | 'Disconnected';
 
 export interface UploadOptions {
-	contentLength?: number;
-	progress?: Progress<{ message?: string; increment?: number }>;
-	token?: CancellationToken;
+    contentLength?: number;
+    progress?: Progress<{ message?: string; increment?: number }>;
+    token?: CancellationToken;
 }
 
 export interface CloudShell {
-	readonly status: CloudShellStatus;
-	readonly onStatusChanged: Event<CloudShellStatus>;
-	readonly waitForConnection: () => Promise<boolean>;
-	readonly terminal: Promise<Terminal>;
-	readonly session: Promise<AzureSession>;
-	readonly uploadFile: (filename: string, stream: ReadStream, options?: UploadOptions) => Promise<void>;
+    readonly status: CloudShellStatus;
+    readonly onStatusChanged: Event<CloudShellStatus>;
+    readonly waitForConnection: () => Promise<boolean>;
+    readonly terminal: Promise<Terminal>;
+    readonly session: Promise<AzureSession>;
+    readonly uploadFile: (filename: string, stream: ReadStream, options?: UploadOptions) => Promise<void>;
 }
