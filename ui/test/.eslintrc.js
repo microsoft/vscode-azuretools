8cc35f4071bd306acf43a01cb50f26a2c5299232
Alex Weininger <alex.weininger@live.com>
2022-02-16 12:40:11 -0800

    Remove ui directory

--- ui/test/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "extends": [
-        "@microsoft/eslint-config-azuretools",
-        "@microsoft/eslint-config-azuretools/test"
-    ],
-    "rules": {
-        "no-restricted-imports": "off"
-    }
-};
