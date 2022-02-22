4b5a2677173b22b2123ea3b3bc727e228e1b2069
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-02-22 13:15:58 -0500

    Remove workaround for SDK issue

--- appservice/src/tryGetSiteResource.ts
+++ appservice/src/tryGetSiteResource.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import type { AppServicePlansGetResponse, DefaultErrorResponse, WebAppsGetResponse, WebAppsGetSlotResponse, WebSiteManagementClient } from '@azure/arm-appservice';
+import type { AppServicePlansGetResponse, WebAppsGetResponse, WebAppsGetSlotResponse, WebSiteManagementClient } from '@azure/arm-appservice';
 import { parseError } from '@microsoft/vscode-azext-utils';
 
 export async function tryGetAppServicePlan(client: WebSiteManagementClient, resourceGroupName: string, name: string): Promise<AppServicePlansGetResponse | undefined> {
@@ -18,21 +18,15 @@ export async function tryGetWebAppSlot(client: WebSiteManagementClient, resource
     return await tryGetSiteResource(async () => await client.webApps.getSlot(resourceGroupName, name, slot));
 }
 
-/**
- * Workaround for https://github.com/Azure/azure-sdk-for-js/issues/10457
- * The azure sdk currently returns the error when a resource isn't found. Instead, they should throw the error or return undefined. We will do the latter.
- *
- * Example values for `result`:
- * 1. { "error": { "code": "ResourceGroupNotFound", "message": "Resource group 'appsvc_linux_centralus' could not be found." }}
- * 2. { "error": { "code": "ResourceNotFound", "message": "The Resource 'Microsoft.Web/serverFarms/appsvc_linux_centralus' under resource group 'appsvc_linux_centralus' was not found. For more details please go to https://aka.ms/ARMResourceNotFoundFix" }}
- * 3. { "Code": "NotFound", "Message": "Server farm with name appsvc_linux_centralus not found." }
- */
-async function tryGetSiteResource<T>(callback: () => Promise<T | DefaultErrorResponse>): Promise<T | undefined> {
-    const result: T | DefaultErrorResponse = await callback();
-    const regExp: RegExp = /NotFound/i;
-    if (regExp.test(parseError(result).errorType) || ('error' in result && regExp.test(parseError(result.error).errorType))) {
-        return undefined;
-    } else {
-        return <T>result;
+async function tryGetSiteResource<T>(callback: () => Promise<T>): Promise<T | undefined> {
+    try {
+        return await callback();
+    } catch (error) {
+        const regExp: RegExp = /NotFound/i;
+        if (regExp.test(parseError(error).errorType)) {
+            return undefined;
+        } else {
+            throw error;
+        }
     }
 }
