d267a25deb3cd29481c9df6ed57843663f1216f5
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-05-06 12:10:27 -0400

    Add option to `pickAppResource` for resolving quick picks ahead of time

--- utils/hostapi.d.ts
+++ utils/hostapi.d.ts
@@ -87,11 +87,18 @@ export interface AzureHostExtensionApi {
 }
 
 export interface AppResourceFilter {
-    type: string;
-    kind?: string;
     /**
-     * ARM tags that must be present on the resource. Resource must match all provided tags.
+     * App resource type to filter the app resource picks
+     */
+    type: string;
 
+    /**
+     * (Optional) App resource kind to filter the app resource picks
+     */
+    kind?: string;
+
+    /**
+     * (Optional) ARM tags that must be present on the resource. Resource must match all provided tags.
      */
     tags?: Record<string, string>;
 }
@@ -106,6 +113,13 @@ export interface PickAppResourceOptions extends IAzureQuickPickOptions {
      * Set this to pick a child of the selected app resource
      */
     expectedChildContextValue?: string | RegExp | (string | RegExp)[];
+
+    /**
+     * Whether `AppResourceTreeItem`s should be resolved before displaying them as quick picks, or only once one has been selected
+     * If a client extension needs to change label/description/something visible on the quick pick via `resolve`, set to true,
+     * otherwise set to false. Default will be false.
+     */
+    resolveQuickPicksBeforeDisplay?: boolean;
 }
 
 /**
