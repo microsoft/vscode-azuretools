08e530d572addfb3bb1e526934538982865ece1e
Alex Weininger <alex.weininger@live.com>
2022-07-13 14:45:12 -0700

    Convert enum to literal union

--- utils/azureResourceType.d.ts
+++ utils/azureResourceType.d.ts
@@ -7,40 +7,40 @@
  * Normalized type for Azure resources that uniquely identifies resource type for the purposes
  * of the Azure extensions
  */
-export declare enum AzureResourceType {
-    AppServices = 'AppServices',
-    StaticWebApps = 'StaticWebApps',
-    FunctionApp = 'FunctionApp',
-    LogicApp = 'LogicApp',
-    VirtualMachines = 'VirtualMachines',
-    StorageAccounts = 'StorageAccounts',
-    NetworkSecurityGroups = 'NetworkSecurityGroups',
-    LoadBalancers = 'LoadBalancers',
-    Disks = 'Disks',
-    Images = 'Images',
-    AvailabilitySets = 'AvailabilitySets',
-    VirtualMachineScaleSets = 'VirtualMachineScaleSets',
-    VirtualNetworks = 'VirtualNetworks',
-    FrontDoorAndCdnProfiles = 'FrontDoorAndCdnProfiles',
-    PublicIpAddresses = 'PublicIpAddresses',
-    NetworkInterfaces = 'NetworkInterfaces',
-    NetworkWatchers = 'NetworkWatchers',
-    BatchAccounts = 'BatchAccounts',
-    ContainerRegistry = 'ContainerRegistry',
-    PostgresqlServersStandard = 'PostgresqlServersStandard',
-    PostgresqlServersFlexible = 'PostgresqlServersFlexible',
-    MysqlServers = 'MysqlServers',
-    SqlDatabases = 'SqlDatabases',
-    SqlServers = 'SqlServers',
-    AzureCosmosDb = 'AzureCosmosDb',
-    OperationalInsightsWorkspaces = 'OperationalInsightsWorkspaces',
-    OperationsManagementSolutions = 'OperationsManagementSolutions',
-    ApplicationInsights = 'ApplicationInsights',
-    AppServicePlans = 'AppServicePlans',
-    AppServiceKubernetesEnvironment = 'AppServiceKubernetesEnvironment',
-    ContainerAppsEnvironment = 'ContainerAppsEnvironment',
-    ContainerApps = 'ContainerApps',
-}
+type AzureResourceBrand = [
+    'AppServices',
+    'StaticWebApps',
+    'FunctionApp',
+    'LogicApp',
+    'VirtualMachines',
+    'StorageAccounts',
+    'NetworkSecurityGroups',
+    'LoadBalancers',
+    'Disks',
+    'Images',
+    'AvailabilitySets',
+    'VirtualMachineScaleSets',
+    'VirtualNetworks',
+    'FrontDoorAndCdnProfiles',
+    'PublicIpAddresses',
+    'NetworkInterfaces',
+    'NetworkWatchers',
+    'BatchAccounts',
+    'ContainerRegistry',
+    'PostgresqlServersStandard',
+    'PostgresqlServersFlexible',
+    'MysqlServers',
+    'SqlDatabases',
+    'SqlServers',
+    'AzureCosmosDb',
+    'OperationalInsightsWorkspaces',
+    'OperationsManagementSolutions',
+    'ApplicationInsights',
+    'AppServicePlans',
+    'AppServiceKubernetesEnvironment',
+    'ContainerAppsEnvironment',
+    'ContainerApps',
+][number];
 
 /**
  * Gets a normalized type for an Azure resource, accounting for the fact that some
@@ -48,4 +48,4 @@ export declare enum AzureResourceType {
  * @param resource The resource to check the {@link AzureResourceType} for
  * @returns The normalized Azure resource type
  */
-export declare function getAzureResourceType(resource: { type: string; kind?: string }): AzureResourceType | undefined;
+export declare function getAzureResourceType(resource: { type: string; kind?: string }): AzureResourceBrand | undefined;
--- utils/src/getAzureResourceType.ts
+++ utils/src/getAzureResourceType.ts
@@ -3,12 +3,12 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import { AzureResourceType } from "../azureResourceType";
+import { AzureResourceBrand } from "../azureResourceType";
 
 const FunctionAppKind = 'functionapp';
 const LogicAppKind = 'workflowapp';
 
-export function getAzureResourceType(resource: { type: string; kind?: string; }): AzureResourceType | undefined {
+export function getAzureResourceType(resource: { type: string; kind?: string; }): AzureResourceBrand | undefined {
     const type = resource.type.toLowerCase();
     const kind = resource.kind?.toLowerCase() || '';
 
@@ -16,99 +16,99 @@ export function getAzureResourceType(resource: { type: string; kind?: string; })
         case 'microsoft.web/sites':
             // Logic apps, function apps, and app services all have the same type
             if (kind.includes(FunctionAppKind) && kind.includes(LogicAppKind)) {
-                return AzureResourceType.LogicApp;
+                return 'LogicApp';
             } else if (kind.includes(FunctionAppKind)) {
-                return AzureResourceType.FunctionApp;
+                return 'FunctionApp';
             } else {
-                return AzureResourceType.AppServices;
+                return 'AppServices';
             }
 
         case 'microsoft.web/staticsites':
-            return AzureResourceType.StaticWebApps;
+            return 'StaticWebApps';
 
         case 'microsoft.compute/virtualmachines':
-            return AzureResourceType.VirtualMachines;
+            return 'VirtualMachines';
 
         case 'microsoft.storage/storageaccounts':
-            return AzureResourceType.StorageAccounts;
+            return 'StorageAccounts';
 
         case 'microsoft.network/networksecuritygroups':
-            return AzureResourceType.NetworkSecurityGroups;
+            return 'NetworkSecurityGroups';
 
         case 'microsoft.network/loadbalancers':
-            return AzureResourceType.LoadBalancers;
+            return 'LoadBalancers';
 
         case 'microsoft.compute/disks':
-            return AzureResourceType.Disks;
+            return 'Disks';
 
         case 'microsoft.compute/images':
-            return AzureResourceType.Images;
+            return 'Images';
 
         case 'microsoft.compute/availabilitysets':
-            return AzureResourceType.AvailabilitySets;
+            return 'AvailabilitySets';
 
         case 'microsoft.compute/virtualmachinescalesets':
-            return AzureResourceType.VirtualMachineScaleSets;
+            return 'VirtualMachineScaleSets';
 
         case 'microsoft.network/virtualnetworks':
-            return AzureResourceType.VirtualNetworks;
+            return 'VirtualNetworks';
 
         case 'microsoft.cdn/profiles':
-            return AzureResourceType.FrontDoorAndCdnProfiles;
+            return 'FrontDoorAndCdnProfiles';
 
         case 'microsoft.network/publicipaddresses':
-            return AzureResourceType.PublicIpAddresses;
+            return 'PublicIpAddresses';
 
         case 'microsoft.network/networkinterfaces':
-            return AzureResourceType.NetworkInterfaces;
+            return 'NetworkInterfaces';
 
         case 'microsoft.network/networkwatchers':
-            return AzureResourceType.NetworkWatchers;
+            return 'NetworkWatchers';
 
         case 'microsoft.batch/batchaccounts':
-            return AzureResourceType.BatchAccounts;
+            return 'BatchAccounts';
 
         case 'microsoft.containerregistry/registries':
-            return AzureResourceType.ContainerRegistry;
+            return 'ContainerRegistry';
 
         case 'microsoft.dbforpostgresql/servers':
-            return AzureResourceType.PostgresqlServersStandard;
+            return 'PostgresqlServersStandard';
 
         case 'microsoft.dbforpostgresql/flexibleservers':
-            return AzureResourceType.PostgresqlServersFlexible;
+            return 'PostgresqlServersFlexible';
 
         case 'microsoft.dbformysql/servers':
-            return AzureResourceType.MysqlServers;
+            return 'MysqlServers';
 
         case 'microsoft.sql/servers/databases':
-            return AzureResourceType.SqlDatabases;
+            return 'SqlDatabases';
 
         case 'microsoft.sql/servers':
-            return AzureResourceType.SqlServers;
+            return 'SqlServers';
 
         case 'microsoft.documentdb/databaseaccounts':
-            return AzureResourceType.AzureCosmosDb;
+            return 'AzureCosmosDb';
 
         case 'microsoft.operationalinsights/workspaces':
-            return AzureResourceType.OperationalInsightsWorkspaces;
+            return 'OperationalInsightsWorkspaces';
 
         case 'microsoft.operationsmanagement/solutions':
-            return AzureResourceType.OperationsManagementSolutions;
+            return 'OperationsManagementSolutions';
 
         case 'microsoft.insights/components':
-            return AzureResourceType.ApplicationInsights;
+            return 'ApplicationInsights';
 
         case 'microsoft.web/serverfarms':
-            return AzureResourceType.AppServicePlans;
+            return 'AppServicePlans';
 
         case 'microsoft.web/kubeenvironments':
-            return AzureResourceType.AppServiceKubernetesEnvironment;
+            return 'AppServiceKubernetesEnvironment';
 
         case 'microsoft.app/managedenvironments':
-            return AzureResourceType.ContainerAppsEnvironment;
+            return 'ContainerAppsEnvironment';
 
         case 'microsoft.app/containerapps':
-            return AzureResourceType.ContainerApps;
+            return 'ContainerApps';
 
         default:
             return undefined;
