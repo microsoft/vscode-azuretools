29e5ea4965b78cda279b29ff0e2d5217bf3ad351
Alex Weininger <alex.weininger@live.com>
2022-08-11 11:53:45 -0700

    Make azExtResourceType optional

--- utils/hostapi.d.ts
+++ utils/hostapi.d.ts
@@ -132,7 +132,7 @@ export interface AppResource {
      * This is a custom property that unambiguously describes the type of an Azure resource for the purposes of the Azure extensions.
      * This accounts for the fact that some Azure resource have overlapping type and kind values.
      */
-    readonly azExtResourceType: AzExtResourceType;
+    readonly azExtResourceType?: AzExtResourceType;
     readonly type: string;
     readonly kind?: string;
     readonly location?: string;
