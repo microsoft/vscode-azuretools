826525f478c9dfa872076012161e098f2213bf34
Alex Weininger <alex.weininger@live.com>
2022-09-22 09:56:32 -0700

    Update variable name

--- utils/src/treev2/quickPickWizard/GenericQuickPickStep.ts
+++ utils/src/treev2/quickPickWizard/GenericQuickPickStep.ts
@@ -68,12 +68,12 @@ export abstract class GenericQuickPickStep<TContext extends types.QuickPickWizar
         const lastPickedItem: unknown | undefined = getLastNode(wizardContext);
 
         // TODO: if `lastPickedItem` is an `AzExtParentTreeItem`, should we clear its cache?
-        const childElements = (await this.treeDataProvider.getChildren(lastPickedItem)) || [];
-        const childItems = await Promise.all(childElements.map(async (childElement: unknown) => await this.treeDataProvider.getTreeItem(childElement)));
-        const childs: [unknown, vscode.TreeItem][] = childElements.map((childElement: unknown, i: number) => [childElement, childItems[i]]);
+        const childNodes = (await this.treeDataProvider.getChildren(lastPickedItem)) || [];
+        const childItems = await Promise.all(childNodes.map(async (childElement: unknown) => await this.treeDataProvider.getTreeItem(childElement)));
+        const childPairs: [unknown, vscode.TreeItem][] = childNodes.map((childElement: unknown, i: number) => [childElement, childItems[i]]);
 
-        const directChoices = childs.filter(([, ti]) => this.isDirectPick(ti));
-        const indirectChoices = childs.filter(([, ti]) => this.isIndirectPick(ti));
+        const directChoices = childPairs.filter(([, ti]) => this.isDirectPick(ti));
+        const indirectChoices = childPairs.filter(([, ti]) => this.isIndirectPick(ti));
 
         let promptChoices: [unknown, vscode.TreeItem][];
         if (directChoices.length === 0) {
