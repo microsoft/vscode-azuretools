2fc9ce7c5670f946e426eb1626bad2842fe20dab
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-10-17 11:53:40 -0400

    Use `EnvironmentCredential`

--- dev/src/TestAuthentication.ts
+++
@@ -1,4 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.md in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
