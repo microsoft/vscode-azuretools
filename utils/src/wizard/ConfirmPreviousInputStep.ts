46d6d467ce8e7868f22033d8d7b8d6067968f5d7
MicroFish91 <mwfisher91@gmail.com>
2022-12-21 15:13:58 -0600

    update types

--- utils/src/wizard/ConfirmPreviousInputStep.ts
+++ utils/src/wizard/ConfirmPreviousInputStep.ts
@@ -3,20 +3,16 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { AzureWizardPromptStep, IActionContext } from "../..";
+import * as types from "../../index";
 import { localize } from "../localize";
+import { AzureWizardPromptStep } from "./AzureWizardPromptStep";
 
-interface IConfirmInputOptions {
-    prompt?: string;
-    isPassword?: boolean;
-}
-
-export class ConfirmPreviousInputStep<T extends IActionContext> extends AzureWizardPromptStep<T> {
-    constructor(private readonly key: string, private readonly options?: IConfirmInputOptions) {
+export class ConfirmPreviousInputStep extends AzureWizardPromptStep<types.IActionContext> {
+    constructor(private readonly key: string, private readonly options?: types.IConfirmInputOptions) {
         super();
     }
 
-    public async prompt(context: T): Promise<void> {
+    public async prompt(context: types.IActionContext): Promise<void> {
         await context.ui.showInputBox({
             prompt: this.options?.prompt || localize('verifyPreviousInput', 'Please confirm by re-entering the previous value.'),
             password: this.options?.isPassword,
@@ -28,7 +24,7 @@ export class ConfirmPreviousInputStep<T extends IActionContext> extends AzureWiz
         return true;
     }
 
-    private validateInput(context: T, value: string | undefined): string | undefined {
+    private validateInput(context: types.IActionContext, value: string | undefined): string | undefined {
         const valueMismatch: string = localize('valueMismatch', 'The entered value does not match the original.');
         return (context[this.key] === value?.trim()) ? undefined : valueMismatch;
     }
