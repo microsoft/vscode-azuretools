5437d2a4d91b2f0fa9fdbaf186e0d5faecd7482c
Alex Weininger <alex.weininger@live.com>
2023-02-06 10:21:19 -0800

    Expose credentialUtils (#1369)

--- utils/src/utils/credentialUtils.ts
+++ utils/src/utils/credentialUtils.ts
@@ -36,8 +36,6 @@ export function createCredential(getSession: (scopes?: string[]) => vscode.Provi
 
 /**
  * Creates a subscription context from an application subscription.
- *
- * TODO: expose these utils and remove duplicate code in resource groups + client extensions
  */
 export function createSubscriptionContext(subscription: AzureSubscription): ISubscriptionContext {
     return {
