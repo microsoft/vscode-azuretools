6961a8acedcd1f37260e9ad7e780d56eacf430f6
Alex Weininger <alex.weininger@live.com>
2023-02-09 09:22:49 -0800

    Add `createPortalUri` util (#1376)
    * Add `createPortalUri` util
    
    * Only import type

---
+++ azure/src/utils/createPortalUri.ts
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import type { AzureSubscription } from "@microsoft/vscode-azureresources-api";
+import { OpenInPortalOptions } from "../../index";
+import * as vscode from 'vscode';
+
+export function createPortalUri(subscription: AzureSubscription, id: string, options?: OpenInPortalOptions): vscode.Uri {
+    const queryPrefix: string = (options && options.queryPrefix) ? `?${options.queryPrefix}` : '';
+    const url: string = `${subscription.environment.portalUrl}/${queryPrefix}#@${subscription.tenantId}/resource${id}`;
+
+    return vscode.Uri.parse(url);
+}
