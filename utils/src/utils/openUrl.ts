f588e75482dfdd371a9379d0dac703108d5b39ed
Nathan Turinski <naturins@microsoft.com>
2023-03-16 10:56:19 -0700

    Implement openUrl to accept string or Uri

--- utils/src/utils/openUrl.ts
+++ utils/src/utils/openUrl.ts
@@ -3,9 +3,14 @@
  *  Licensed under the MIT License. See License.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as vscode from 'vscode';
+import { Uri, env } from 'vscode';
 
-export async function openUrl(url: string): Promise<void> {
-    // Using this functionality is blocked by https://github.com/Microsoft/vscode/issues/85930
-    await vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(url));
+export async function openUrl(url: string | Uri): Promise<void> {
+    // Workaround according to https://github.com/Microsoft/vscode/issues/85930
+    // When a string is passed to openExternal, it is not encoded by VS Code which seems
+    // to be what is causing issues with some URLs.
+    // https://github.com/microsoft/vscode/blob/2edb004e8386b2a86b26e8f30ea6969f4f26ffa7/src/vs/editor/browser/services/openerService.ts#L223-L226
+    // So if a URI is causing errors, try passing a string
+
+    await env.openExternal(url as unknown as Uri);
 }
