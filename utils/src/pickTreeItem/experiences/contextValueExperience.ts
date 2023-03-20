458c6e2c0b8bf3c4b20ebd9e9ccdd27188b42a93
Alex Weininger <alex.weininger@live.com>
2023-03-20 15:16:54 -0400

    utils: use `runQuickPickWizard` in experiences

--- utils/src/pickTreeItem/experiences/contextValueExperience.ts
+++ utils/src/pickTreeItem/experiences/contextValueExperience.ts
@@ -6,12 +6,9 @@
 import * as types from '../../../index';
 import * as vscode from 'vscode';
 import { RecursiveQuickPickStep } from '../contextValue/RecursiveQuickPickStep';
-import { getLastNode } from '../getLastNode';
-import { NoResourceFoundError } from '../../errors';
 import { AzureWizardPromptStep } from '../../wizard/AzureWizardPromptStep';
-import { AzureWizard } from '../../wizard/AzureWizard';
 import { ResourceGroupsItem } from '../quickPickAzureResource/tempTypes';
-import { isWrapper } from '@microsoft/vscode-azureresources-api';
+import { runQuickPickWizard } from '../runQuickPickWizard';
 
 export async function contextValueExperience<TPick>(context: types.IActionContext, tdp: vscode.TreeDataProvider<ResourceGroupsItem>, contextValueFilter: types.ContextValueFilter): Promise<TPick> {
     const promptSteps: AzureWizardPromptStep<types.QuickPickWizardContext>[] = [
@@ -21,22 +18,8 @@ export async function contextValueExperience<TPick>(context: types.IActionContex
         }),
     ];
 
-    // Fill in the `pickedNodes` property
-    const wizardContext = { ...context } as types.QuickPickWizardContext;
-    wizardContext.pickedNodes = [];
-
-    const wizard = new AzureWizard(wizardContext, {
+    return await runQuickPickWizard(context, {
         hideStepCount: true,
         promptSteps: promptSteps,
     });
-
-    await wizard.prompt();
-
-    const lastPickedItem = getLastNode(wizardContext);
-
-    if (!lastPickedItem) {
-        throw new NoResourceFoundError(wizardContext);
-    } else {
-        return isWrapper(lastPickedItem) ? lastPickedItem.unwrap<TPick>() : lastPickedItem as unknown as TPick;
-    }
 }
