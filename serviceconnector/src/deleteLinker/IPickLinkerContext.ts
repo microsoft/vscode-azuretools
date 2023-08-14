44c3d558bdd932566fbdea3e3157149066504a76
Megan Mott <meganmott@microsoft.com>
2023-08-14 16:51:59 -0700

    Add children to validate activity log

--- serviceconnector/src/deleteLinker/IPickLinkerContext.ts
+++ serviceconnector/src/deleteLinker/IPickLinkerContext.ts
@@ -3,9 +3,14 @@
 *  Licensed under the MIT License. See License.md in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
+import { ValidationResultItem } from "@azure/arm-servicelinker";
+import { ExecuteActivityContext } from "@microsoft/vscode-azext-utils";
 import { ICreateLinkerContext } from "../createLinker/ICreateLinkerContext";
 
-export interface IPickLinkerContext extends ICreateLinkerContext {
+export interface IPickLinkerContext extends ICreateLinkerContext, ExecuteActivityContext {
     linkerName?: string;
+
+    //Validation
+    validationResult?: ValidationResultItem[]
 }
 
