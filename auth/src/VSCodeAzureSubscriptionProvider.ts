2c73703582649f5ec40bfa520b0c926054e93905
Nathan <naturins@microsoft.com>
2023-10-30 16:03:04 -0700

    auth: Hard-code node-fetch version (#1615)
    * Add node-fetch and use it for tenant list step
    
    * Add comment
    
    * Revert version number

--- auth/src/VSCodeAzureSubscriptionProvider.ts
+++ auth/src/VSCodeAzureSubscriptionProvider.ts
@@ -5,6 +5,7 @@
 
 import type { SubscriptionClient, TenantIdDescription } from '@azure/arm-subscriptions'; // Keep this as `import type` to avoid actually loading the package before necessary
 import type { TokenCredential } from '@azure/core-auth'; // Keep this as `import type` to avoid actually loading the package (at all, this one is dev-only)
+import fetch from 'node-fetch'; // have to explicitly use node-fetch v2.6.7 otherwise when @azure/client-core makes a streaming request, it fails on windows
 import * as vscode from 'vscode';
 import type { AzureAuthentication } from './AzureAuthentication';
 import type { AzureSubscription, SubscriptionId, TenantId } from './AzureSubscription';
