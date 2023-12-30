f5a3d2ce121f440c7bee5668b5ad07e97bd8afad
MicroFish91 <mwfisher91@gmail.com>
2023-12-29 22:21:40 -0600

    Formatting

--- utils/test/validationUtils/hasValidCharLength.test.ts
+++ utils/test/validationUtils/hasValidCharLength.test.ts
@@ -13,12 +13,12 @@ type CharLengthParams = {
 
 export function hasValidCharLengthTest() {
     const trueValues: CharLengthParams[] = [
-        { value: 'test' }, // No limits specified
-        { value: 'abcdef', constraints: { lowerLimitIncl: undefined, upperLimitIncl: 10 } }, // No lower limit specified
-        { value: 'abcdefghijklmnopqrstuvwxyz', constraints: { lowerLimitIncl: 1 } }, // No upper limit specified
-        { value: 'a', constraints: { lowerLimitIncl: 1, upperLimitIncl: 10 } },  // At lower limit
-        { value: '1234567890', constraints: { lowerLimitIncl: 1, upperLimitIncl: 10 } }, // At upper limit
-        { value: 'abcd', constraints: { lowerLimitIncl: 1, upperLimitIncl: 10 } }, // Within limits
+        { value: 'test' },
+        { value: 'abcdef', constraints: { lowerLimitIncl: undefined, upperLimitIncl: 10 } },
+        { value: 'abcdefghijklmnopqrstuvwxyz', constraints: { lowerLimitIncl: 1 } },
+        { value: 'a', constraints: { lowerLimitIncl: 1, upperLimitIncl: 10 } },
+        { value: '1234567890', constraints: { lowerLimitIncl: 1, upperLimitIncl: 10 } },
+        { value: 'abcd', constraints: { lowerLimitIncl: 1, upperLimitIncl: 10 } },
     ];
 
     for (const { value, constraints } of trueValues) {
@@ -27,10 +27,10 @@ export function hasValidCharLengthTest() {
 
     const falseValues: CharLengthParams[] = [
         { value: '' },
-        { value: '', constraints: { lowerLimitIncl: 0, upperLimitIncl: 1 } }, // Lower limit != 0
-        { value: 'abcdefg', constraints: { lowerLimitIncl: 5, upperLimitIncl: -5 } },  // Lower limit > upper limit
-        { value: 'abc', constraints: { lowerLimitIncl: 4, upperLimitIncl: 10 } }, // Below limit
-        { value: '12345678901', constraints: { lowerLimitIncl: 1, upperLimitIncl: 10 } },  // Above limit
+        { value: '', constraints: { lowerLimitIncl: 0, upperLimitIncl: 1 } },
+        { value: 'abcdefg', constraints: { lowerLimitIncl: 5, upperLimitIncl: -5 } },
+        { value: 'abc', constraints: { lowerLimitIncl: 4, upperLimitIncl: 10 } },
+        { value: '12345678901', constraints: { lowerLimitIncl: 1, upperLimitIncl: 10 } },
     ];
 
     for (const { value, constraints } of falseValues) {
