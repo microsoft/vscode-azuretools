5ddc769aa9c998fcdfd98a1b9b4d4e577d925fff
Nathan <naturins@microsoft.com>
2024-01-22 17:42:13 -0800

    appservice: Refactor deployment code into steps (#1554)
    * Refactor deployment code into steps
    
    * Small fixes
    
    * Forgot DelayFirstWebAppDeployStep. Make aspPromise on the context
    
    * Save the file
    
    * PR feedback
    
    * Fixin' da lint man
    
    * Save file
    
    * Weirdness with files not saving
    
    * Merge from main
    
    * Delete DeployGithubExecuteStep.ts
    
    * Delete appservice/src/deploy/wizard/DeployExecuteBaseStep.ts
    
    * Deploy with flexconsumption
    
    * Rename DeployExecuteStepBase from DeployExecuteBaseStep
    
    * Bump appservice version for breaking change

--- appservice/src/deploy/IDeployContext.ts
+++ appservice/src/deploy/IDeployContext.ts
@@ -3,8 +3,10 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { IActionContext } from '@microsoft/vscode-azext-utils';
+import { AppServicePlan } from '@azure/arm-appservice';
+import { ExecuteActivityContext, IActionContext } from '@microsoft/vscode-azext-utils';
 import { WorkspaceFolder } from 'vscode';
+import { ParsedSite, SiteClient } from '../SiteClient';
 
 export enum AppSource {
     setting = 'setting',
@@ -29,4 +31,16 @@ export interface IDeployContext extends IActionContext {
     flexConsumptionRemoteBuild?: boolean;
     stopAppBeforeDeploy?: boolean;
     syncTriggersPostDeploy?: boolean;
+    /**
+     * id retrieved from scm-deployment-id header to track deployment
+     */
+    locationUrl?: string;
+}
+
+// only used by the tools package facilitate creating the wizard execute steps
+export interface InnerDeployContext extends IDeployContext, ExecuteActivityContext {
+    site: ParsedSite;
+    client: SiteClient;
+    fsPath: string;
+    aspPromise: Promise<AppServicePlan | undefined>
 }
