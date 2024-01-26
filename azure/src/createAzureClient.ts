e1cd099ad47fe0d2989e2332a5e9b7395caaaa04
Alex Weininger <alex.weininger@live.com>
2024-01-26 09:27:59 -0800

    azure: use management endpoint in `createGenericClient` (#1670)
    * azure: use management endpoint in `createGenericClient`
    
    * Bump version

--- azure/src/createAzureClient.ts
+++ azure/src/createAzureClient.ts
@@ -73,7 +73,9 @@ export async function createGenericClient(context: IActionContext, clientInfo: t
     let endpoint: string | undefined;
     if (clientInfo && 'credentials' in clientInfo) {
         credentials = clientInfo.credentials;
-        endpoint = clientInfo.environment.resourceManagerEndpointUrl;
+        // managementEndpointUrl needs to be used instead of resourceManagerEndpointUrl to fix soverign cloud support
+        // see https://github.com/microsoft/vscode-azuretools/pull/1669
+        endpoint = clientInfo.environment.managementEndpointUrl;
     } else {
         credentials = clientInfo;
     }
