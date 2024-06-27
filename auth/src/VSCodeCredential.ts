2c4d7714befdaec2fe58115322b0a3f0aad474a6
alexweininger <alex.weininger@live.com>
2024-06-27 09:44:53 -0700

    add VSCodeCredential

--- auth/src/VSCodeCredential.ts
+++ auth/src/VSCodeCredential.ts
@@ -5,7 +5,7 @@ import { getSessionFromVSCode } from "./getSessionFromVSCode";
  * For use with Azure SDKs
  */
 export class VSCodeAzureSDKCredential implements TokenCredential {
-    public async getToken(scopes: string | string[], options?: GetTokenOptions | undefined): Promise<AccessToken | null> {
+    public async getToken(scopes: string[], options?: GetTokenOptions | undefined): Promise<AccessToken | null> {
         const session = await getSessionFromVSCode(scopes, options?.tenantId, { createIfNone: true });
         return {
             token: session?.accessToken ?? "",
