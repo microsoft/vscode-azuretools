bbd9dbf17a617eccff1f425a486ecf6310458ddb
Nathan <naturins@microsoft.com>
2024-08-06 10:41:58 -0700

    auth: Add a `getSessionWithScopes` to AzureAuthentication (#1762)
    * Add a getSessionWithScopes to AzureAuthentication
    
    * Remove scopes as a parameter
    
    * Bump package version
    
    * Fix build and PR feedback
    
    * Actually listen to PR feedback

--- auth/src/AzureAuthentication.ts
+++ auth/src/AzureAuthentication.ts
@@ -11,10 +11,17 @@ import type * as vscode from 'vscode';
 export interface AzureAuthentication {
     /**
      * Gets a VS Code authentication session for an Azure subscription.
+     * Always uses the default scope, `https://management.azure.com/.default/` and respects `microsoft-sovereign-cloud.environment` setting.
+     *
+     * @returns A VS Code authentication session or undefined, if none could be obtained.
+     */
+    getSession(): vscode.ProviderResult<vscode.AuthenticationSession>;
+    /**
+     * Gets a VS Code authentication session for an Azure subscription.
      *
      * @param scopes - The scopes for which the authentication is needed.
      *
      * @returns A VS Code authentication session or undefined, if none could be obtained.
      */
-    getSession(scopes?: string[]): vscode.ProviderResult<vscode.AuthenticationSession>;
+    getSessionWithScopes(scopes: string[]): vscode.ProviderResult<vscode.AuthenticationSession>;
 }
