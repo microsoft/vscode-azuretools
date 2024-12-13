61757d2b28baac0ef0fae29e7ef528307f1ffa9e
Megan Mott <meganmott@microsoft.com>
2024-12-13 14:47:32 -0800

    use account property

--- auth/src/utils/getUnauthenticatedTenants.ts
+++ auth/src/utils/getUnauthenticatedTenants.ts
@@ -4,24 +4,18 @@
 *--------------------------------------------------------------------------------------------*/
 
 import type { TenantIdDescription } from "@azure/arm-resources-subscriptions";
-import * as vscode from "vscode";
 import type { AzureSubscriptionProvider } from "../AzureSubscriptionProvider";
-import { getConfiguredAuthProviderId } from "./configuredAzureEnv";
 
 /**
  * @returns list of tenants that VS Code doesn't have sessions for
  */
 export async function getUnauthenticatedTenants(subscriptionProvider: AzureSubscriptionProvider): Promise<TenantIdDescription[]> {
-    const accounts = Array.from((await vscode.authentication.getAccounts(getConfiguredAuthProviderId()))).sort((a, b) => a.label.localeCompare(b.label));
     const unauthenticatedTenants: TenantIdDescription[] = [];
-
-    for (const account of accounts) {
-        const tenants = await subscriptionProvider.getTenants(account);
-        for await (const tenant of tenants) {
-            if (tenant) {
-                if (!await subscriptionProvider.isSignedIn(tenant.tenantId, account)) {
-                    unauthenticatedTenants.push(tenant);
-                }
+    const tenants = await subscriptionProvider.getTenants();
+    for await (const tenant of tenants) {
+        if (tenant) {
+            if (!await subscriptionProvider.isSignedIn(tenant.tenantId, tenant.account)) {
+                unauthenticatedTenants.push(tenant);
             }
         }
     }
