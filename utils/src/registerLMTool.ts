a1ce5e179073b0ff7c0c94672290ea452bffd06a
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-03-06 16:35:37 -0500

    Add `registerLMTool` function

---
+++ utils/src/registerLMTool.ts
@@ -0,0 +1,32 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import * as types from '../index';
+import { callWithTelemetryAndErrorHandling } from './callWithTelemetryAndErrorHandling';
+import { ext } from './extensionVariables';
+
+export function registerLMTool<T>(toolId: string, tool: types.AzExtLMTool<T>): void {
+    const vscodeTool: vscode.LanguageModelTool<T> = {
+        invoke: async (options, token) => {
+            return await callWithTelemetryAndErrorHandling(`${toolId}.invoke`, async (context: types.IActionContext) => {
+                return await tool.invoke(context, options, token);
+            }) as vscode.LanguageModelToolResult;
+        }
+    };
+
+    if (tool.prepareInvocation) {
+        vscodeTool.prepareInvocation = async (options, token) => {
+            return await callWithTelemetryAndErrorHandling(`${toolId}.prepareInvocation`, async (context: types.IActionContext) => {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                return await tool.prepareInvocation!(context, options, token);
+            })
+        }
+    }
+
+    ext.context.subscriptions.push(
+        vscode.lm.registerTool(toolId, vscodeTool)
+    );
+}
