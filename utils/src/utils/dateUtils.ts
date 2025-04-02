c02aeadff94b6bcaf739aaa03fb15d4158998e6d
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-04-01 20:37:30 -0700

    fix regex

--- utils/src/utils/dateUtils.ts
+++ utils/src/utils/dateUtils.ts
@@ -17,7 +17,7 @@ export namespace dateUtils {
     export function getFormattedDurationInMinutesAndSeconds(start: Date, end: Date): string {
         const d: duration.Duration = dayjs.duration(end.getTime() - start.getTime());
         return `${d.minutes()}m ${d.seconds()}s`
-            .replace('\b0m\b', '')
-            .replace('\b0s\b', '');
+            .replace(/\b0m\b/, '')
+            .replace(/\b0s\b/, '');
     }
 }
