665869e8b5a816e6b5e8f063a3767da6d670aecc
alexweininger <alex.weininger@live.com>
2025-08-19 15:10:50 -0400

    azure: fix space

--- azure/src/createAzureClient.ts
+++ azure/src/createAzureClient.ts
@@ -285,7 +285,6 @@ class StatusCodePolicy implements PipelinePolicy {
     }
 }
 
-
 /**
  * Encodes userName and password and signs a request with the Authentication header.
  * Imitates BasicAuthenticationCredentials from ms-rest-js
