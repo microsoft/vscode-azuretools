38791dc68b3497ad0b68643edfbaaedb338705ba
alexweininger <alex.weininger@live.com>
2025-08-20 15:58:29 -0400

    Add copyright header

--- auth/src/utils/isAuthenticationSessionRequest.ts
+++ auth/src/utils/isAuthenticationSessionRequest.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
 import * as vscode from 'vscode';
 
 export function isAuthenticationSessionRequest(scopes?: string | string[] | vscode.AuthenticationSessionRequest): scopes is vscode.AuthenticationSessionRequest {
