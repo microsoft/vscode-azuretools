799496d5ef1e69cd46c1a72d18122f3bdbb0b2d5
Megan Mott <59709511+motm32@users.noreply.github.com>
2025-09-24 09:57:04 -0700

    appservice: Update App Service SKU's (#2084)
    * initial
    
    * add descriptions
    
    * change labels and bumo version

--- appservice/src/createAppService/AppServicePlanSkuStep.ts
+++ appservice/src/createAppService/AppServicePlanSkuStep.ts
@@ -3,9 +3,11 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import type { SkuDescription } from '@azure/arm-appservice';
+import type { GeoRegion, SkuDescription } from '@azure/arm-appservice';
+import { LocationListStep, uiUtils } from '@microsoft/vscode-azext-azureutils';
 import { AzureWizardPromptStep, IAzureQuickPickItem, nonNullProp } from '@microsoft/vscode-azext-utils';
 import * as vscode from 'vscode';
+import { createWebSiteClient } from '../utils/azureClients';
 import { openUrl } from '../utils/openUrl';
 import { AppKind, WebsiteOS } from './AppKind';
 import { IAppServiceWizardContext } from './IAppServiceWizardContext';
@@ -15,7 +17,13 @@ type ExtendedSkuDescription = SkuDescription & { label?: string; description?: s
 
 export class AppServicePlanSkuStep extends AzureWizardPromptStep<IAppServiceWizardContext> {
     public async prompt(context: IAppServiceWizardContext): Promise<void> {
-        let skus: ExtendedSkuDescription[] = context.advancedCreation ? this.getRecommendedSkus().concat(this.getAdvancedSkus()) : this.getRecommendedSkus();
+        let skus: ExtendedSkuDescription[] = [];
+        if (await this.isPV4Region(context)) {
+            skus = context.advancedCreation ? this.pv4RecommendedSkus().concat(this.getAdvancedSkus()) : this.pv4RecommendedSkus();
+        } else {
+            skus = context.advancedCreation ? this.nonPV4RecommendedSkus().concat(this.getAdvancedSkus()) : this.nonPV4RecommendedSkus();
+        }
+
         if (context.newSiteKind === AppKind.functionapp) {
             skus.push(...this.getElasticPremiumSkus());
         } else if (context.newSiteKind?.includes(AppKind.workflowapp)) {
@@ -34,7 +42,7 @@ export class AppServicePlanSkuStep extends AzureWizardPromptStep<IAppServiceWiza
         const pricingTiers: IAzureQuickPickItem<SkuDescription | undefined>[] = skus.map(s => {
             return {
                 label: s.label || nonNullProp(s, 'name'),
-                description: s.description || s.tier,
+                description: s.description,
                 data: s,
                 group: s.group || vscode.l10n.t('Additional Options')
             };
@@ -62,7 +70,23 @@ export class AppServicePlanSkuStep extends AzureWizardPromptStep<IAppServiceWiza
         return !context.newPlanSku;
     }
 
-    private getRecommendedSkus(): ExtendedSkuDescription[] {
+    private async isPV4Region(context: IAppServiceWizardContext): Promise<boolean> {
+        const client = await createWebSiteClient(context);
+        const geoRegions: GeoRegion[] = await uiUtils.listAllIterator(client.listGeoRegions());
+        const pv4Regions = geoRegions.filter((region: GeoRegion) => region.orgDomain?.includes('PV4SERIES'));
+        if (LocationListStep.hasLocation(context)) {
+            const location = await LocationListStep.getLocation(context);
+            if (pv4Regions.some(region => region.displayName === location.displayName)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    private pv4RecommendedSkus(): ExtendedSkuDescription[] {
         const recommendedGroup: string = vscode.l10n.t('Recommended');
         return [
             {
@@ -86,64 +110,161 @@ export class AppServicePlanSkuStep extends AzureWizardPromptStep<IAppServiceWiza
                 group: recommendedGroup
             },
             {
-                name: 'P1v2',
-                tier: 'Premium V2',
-                size: 'P1v2',
-                family: 'Pv2',
+                name: 'P0V4',
+                tier: 'Premium V4',
+                size: 'P0V4',
+                family: 'Pv4',
                 capacity: 1,
-                label: vscode.l10n.t('Premium (P1v2)'),
-                description: vscode.l10n.t('Use in production'),
+                label: vscode.l10n.t('Premium V4 (P0V4)'),
+                group: recommendedGroup
+            },
+            {
+                name: 'P1V4',
+                tier: 'Premium V4',
+                size: 'P1V4',
+                family: 'Pv4',
+                capacity: 1,
+                label: vscode.l10n.t('Premium V4 (P1V4)'),
+                group: recommendedGroup
+            },
+            {
+                name: 'P1MV4',
+                tier: 'Premium V4',
+                size: 'P1MV4',
+                family: 'Pv4',
+                capacity: 1,
+                label: vscode.l10n.t('Premium V4 (P1MV4)'),
+                group: recommendedGroup
+            },
+            {
+                name: 'P0V3',
+                tier: 'Premium V3',
+                size: 'P0V3',
+                family: 'Pv3',
+                capacity: 1,
+                label: vscode.l10n.t('Premium V3 (P0V3)'),
+                group: recommendedGroup
+            },
+            {
+                name: 'P1V3',
+                tier: 'Premium V3',
+                size: 'P1V3',
+                family: 'Pv3',
+                capacity: 1,
+                label: vscode.l10n.t('Premium V3 (P1V3)'),
+                group: recommendedGroup
+            },
+            {
+                name: 'P1MV3',
+                tier: 'Premium V3',
+                size: 'P1MV3',
+                family: 'Pv3',
+                capacity: 1,
+                label: vscode.l10n.t('Premium V3 (P1MV3)'),
                 group: recommendedGroup
             }
-        ];
+        ]
+    }
+
+    private nonPV4RecommendedSkus(): ExtendedSkuDescription[] {
+        const recommendedGroup: string = vscode.l10n.t('Recommended');
+        return [
+            {
+                name: 'F1',
+                tier: 'Free',
+                size: 'F1',
+                family: 'F',
+                capacity: 1,
+                label: vscode.l10n.t('Free (F1)'),
+                description: vscode.l10n.t('Try out Azure at no cost'),
+                group: recommendedGroup
+            },
+            {
+                name: 'B1',
+                tier: 'Basic',
+                size: 'B1',
+                family: 'B',
+                capacity: 1,
+                label: vscode.l10n.t('Basic (B1)'),
+                description: vscode.l10n.t('Develop and test'),
+                group: recommendedGroup
+            },
+            {
+                name: 'P0V3',
+                tier: 'Premium V3',
+                size: 'P0V3',
+                family: 'Pv3',
+                capacity: 1,
+                label: vscode.l10n.t('Premium V3 (P0V3)'),
+                group: recommendedGroup
+            },
+            {
+                name: 'P1V3',
+                tier: 'Premium V3',
+                size: 'P1V3',
+                family: 'Pv3',
+                capacity: 1,
+                label: vscode.l10n.t('Premium V3 (P1V3)'),
+                group: recommendedGroup
+            },
+            {
+                name: 'P1MV3',
+                tier: 'Premium V3',
+                size: 'P1MV3',
+                family: 'Pv3',
+                capacity: 1,
+                label: vscode.l10n.t('Premium V3 (P1MV3)'),
+                group: recommendedGroup
+            }
+        ]
     }
 
     private getAdvancedSkus(): SkuDescription[] {
         return [
             {
-                name: 'B2',
+                name: 'Basic (B2)',
                 tier: 'Basic',
                 size: 'B2',
                 family: 'B',
                 capacity: 1
             },
             {
-                name: 'B3',
+                name: 'Basic (B3)',
                 tier: 'Basic',
                 size: 'B3',
                 family: 'B',
                 capacity: 1
             },
             {
-                name: 'S1',
+                name: 'Standard (S1)',
                 tier: 'Standard',
                 size: 'S1',
                 family: 'S',
                 capacity: 1
             },
             {
-                name: 'S2',
+                name: 'Standard (S2)',
                 tier: 'Standard',
                 size: 'S2',
                 family: 'S',
                 capacity: 1
             },
             {
-                name: 'S3',
+                name: 'Standard (S3)',
                 tier: 'Standard',
                 size: 'S3',
                 family: 'S',
                 capacity: 1
             },
             {
-                name: 'P2v2',
+                name: 'Premium V2 (P2v2)',
                 tier: 'Premium V2',
                 size: 'P2v2',
                 family: 'Pv2',
                 capacity: 1
             },
             {
-                name: 'P3v2',
+                name: 'Premium V2 (P3v2)',
                 tier: 'Premium V2',
                 size: 'P3v2',
                 family: 'Pv2',
@@ -155,21 +276,21 @@ export class AppServicePlanSkuStep extends AzureWizardPromptStep<IAppServiceWiza
     private getPremiumV3Skus(): SkuDescription[] {
         return [
             {
-                name: 'P1v3',
+                name: 'Premium V3 (P1v3)',
                 tier: 'Premium V3',
                 size: 'P1v3',
                 family: 'Pv3',
                 capacity: 1
             },
             {
-                name: 'P2v3',
+                name: 'Premium V3 (P2v3)',
                 tier: 'Premium V3',
                 size: 'P2v3',
                 family: 'Pv3',
                 capacity: 1
             },
             {
-                name: 'P3v3',
+                name: 'Premium V3 (P3v3)',
                 tier: 'Premium V3',
                 size: 'P3v3',
                 family: 'Pv3',
@@ -181,21 +302,21 @@ export class AppServicePlanSkuStep extends AzureWizardPromptStep<IAppServiceWiza
     private getElasticPremiumSkus(): SkuDescription[] {
         return [
             {
-                name: 'EP1',
+                name: 'Elastic Premium (EP1)',
                 tier: 'Elastic Premium',
                 size: 'EP1',
                 family: 'EP',
                 capacity: 1
             },
             {
-                name: 'EP2',
+                name: 'Elastic Premium (EP2)',
                 tier: 'Elastic Premium',
                 size: 'EP2',
                 family: 'EP',
                 capacity: 1
             },
             {
-                name: 'EP3',
+                name: 'Elastic Premium (EP3)',
                 tier: 'Elastic Premium',
                 size: 'EP3',
                 family: 'EP',
@@ -207,21 +328,21 @@ export class AppServicePlanSkuStep extends AzureWizardPromptStep<IAppServiceWiza
     private getWorkflowStandardSkus(): SkuDescription[] {
         return [
             {
-                name: 'WS1',
+                name: 'Workflow Standard WS1',
                 tier: 'Workflow Standard',
                 size: 'WS1',
                 family: 'WS',
                 capacity: 1
             },
             {
-                name: 'WS2',
+                name: 'Workflow Standard WS2',
                 tier: 'Workflow Standard',
                 size: 'WS2',
                 family: 'WS',
                 capacity: 1
             },
             {
-                name: 'WS3',
+                name: 'Workflow Standard WS3',
                 tier: 'Workflow Standard',
                 size: 'WS3',
                 family: 'WS',
