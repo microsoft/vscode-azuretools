aa48fd6f1fbbea10d558ea11133117b9dda4a15e
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-08 09:51:42 -0400

    Initial commit of new eng package

---
+++ eng/README.md
@@ -0,0 +1,7 @@
+# Shared Engineering Package for Azure Visual Studio Code Extensions
+
+This package provides shared engineering dependencies for the Azure extensions for Visual Studio Code.
+
+## License
+
+[MIT](LICENSE.md)
---
+++ eng/src/eslint/eslintConfigs.ts
@@ -0,0 +1,116 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import type { ConfigObject as EslintConfig } from '@eslint/core';
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+/**
+ * Universal rules that should apply to all projects
+ * @note This is exported but not meant to be used in isolation, but rather as a building block for other configs
+ */
+export const azExtUniversalRules: EslintConfig = {
+    ignores: ['out/**', 'node_modules/**', 'dist/**', '**/*.d.ts', '.vscode-test/**', 'eslint.config.mjs'],
+    languageOptions: {
+        parserOptions: {
+            projectService: true,
+        },
+    },
+    rules: {
+        /* eslint-disable @typescript-eslint/naming-convention */
+        curly: 'warn',
+        eqeqeq: 'warn',
+        'no-extra-boolean-cast': 'off',
+        'no-template-curly-in-string': 'warn',
+        semi: 'warn',
+        /* eslint-enable @typescript-eslint/naming-convention */
+    },
+};
+
+/**
+ * Stylistic rules that should apply to all projects
+ * @note This is exported but not meant to be used in isolation, but rather as a building block for other configs
+ */
+export const azExtStylisticRules: EslintConfig = {
+    rules: {
+        /* eslint-disable @typescript-eslint/naming-convention */
+        '@typescript-eslint/naming-convention': [
+            // Naming is enforced with some exceptions below
+            'warn',
+            {
+                // Names should be either camelCase or PascalCase, both are extensively used throughout our projects
+                selector: 'default',
+                format: ['camelCase', 'PascalCase'],
+            },
+            {
+                // const variables can also have UPPER_CASE
+                selector: 'variable',
+                modifiers: ['const'],
+                format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+            },
+            {
+                // private class properties can also have leading _underscores
+                selector: 'classProperty',
+                modifiers: ['private'],
+                format: ['camelCase', 'PascalCase'],
+                leadingUnderscore: 'allow',
+            }
+        ],
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+                // As a function parameter, unused parameters are allowed
+                args: 'none',
+            }
+        ],
+        /* eslint-enable @typescript-eslint/naming-convention */
+    },
+};
+
+/**
+ * Recommended ESLint configuration for Azure extensions
+ */
+export const azextEslintRecommended: EslintConfig[] = defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    tseslint.configs.stylistic,
+    azExtUniversalRules,
+    azExtStylisticRules,
+);
+
+/**
+ * Recommended ESLint configuration for Azure extensions with extra type-checked rules enabled
+ */
+export const azExtEslintRecommendedTypeChecked: EslintConfig[] = defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    azExtUniversalRules,
+    azExtStylisticRules,
+);
+
+/**
+ * Strict ESLint configuration for Azure extensions
+ */
+export const azExtEslintStrict: EslintConfig[] = defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+    azExtUniversalRules,
+    azExtStylisticRules,
+);
+
+/**
+ * Strict ESLint configuration for Azure extensions with extra type-checked rules enabled
+ */
+export const azExtEslintStrictTypeChecked: EslintConfig[] = defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    azExtUniversalRules,
+    azExtStylisticRules,
+);
