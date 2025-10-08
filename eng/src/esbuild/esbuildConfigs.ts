8278850021825f0bbff0aff7410b648cf71227cb
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-08 13:10:19 -0400

    Comments and improvements

--- eng/src/esbuild/esbuildConfigs.ts
+++ eng/src/esbuild/esbuildConfigs.ts
@@ -3,10 +3,18 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+/**
+ * @important When updating these configs, be sure to update the corresponding webpack configs in file://./../webpack/webpackConfigs.ts
+ */
+
 import type { BuildOptions } from 'esbuild';
 import { copy } from 'esbuild-plugin-copy';
 
-const baseConfig: BuildOptions = {
+/**
+ * Base config - shared between prod/dev/debug
+ * @note This is exported but not meant to be used in isolation, but rather as a building block for other configs
+ */
+export const baseConfig: BuildOptions = {
     bundle: true,
     external: ['vscode'],
     outdir: './dist',
@@ -27,18 +35,28 @@ const baseConfig: BuildOptions = {
     ],
 };
 
+/**
+ * Production config - minified, no sourcemap
+ */
 export const azExtEsbuildConfigProd: BuildOptions = {
     ...baseConfig,
     minify: true,
     sourcemap: false,
 };
 
+/**
+ * Dev config - not minified, linked sourcemap
+ */
 export const azExtEsbuildConfigDev: BuildOptions = {
     ...baseConfig,
     minify: false,
     sourcemap: 'linked',
 };
 
+/**
+ * Debug config - not minified, linked sourcemap, metafile
+ * @note To use the metafile, it also needs to be written to disk. See https://esbuild.github.io/api/#metafile
+ */
 export const azExtEsbuildConfigDebug: BuildOptions = {
     ...azExtEsbuildConfigDev,
     metafile: true,
