3df6fce72025b796857b70c58678319196c1de51
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-09 11:18:40 -0400

    Multiple configs for test

--- eng/src/vscode-test/vscodeTestConfigs.ts
+++ eng/src/vscode-test/vscodeTestConfigs.ts
@@ -3,16 +3,35 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { defineConfig } from '@vscode/test-cli';
+import { defineConfig, type TestConfiguration } from '@vscode/test-cli';
 
 /**
- * Default test config for Azure extensions for VS Code.
- * @see https://github.com/microsoft/vscode-test-cli for more options
+ * Base config - shared between different test configs
+ * @note This is exported but not meant to be used in isolation, rather as a building block for other configs
  */
-export const azExtTestConfig = defineConfig({
-    files: 'src/test/**/*.test.{ts,mts,cts}',
+export const baseConfig: Partial<TestConfiguration> = {
     mocha: {
         require: ['tsx'],
         timeout: 10000,
     },
+};
+
+/**
+ * Default `./src/test` config for Azure extensions for VS Code.
+ * @see https://github.com/microsoft/vscode-test-cli for more options
+ */
+export const azExtSrcTestConfig = defineConfig({
+    ...baseConfig,
+    files: 'src/test/**/*.test.{ts,mts,cts}',
+    workspaceFolder: 'src/test/test.code-workspace',
+});
+
+/**
+ * Default `./test` config for Azure extensions for VS Code.
+ * @see https://github.com/microsoft/vscode-test-cli for more options
+ */
+export const azExtTestConfig = defineConfig({
+    ...baseConfig,
+    files: 'test/**/*.test.{ts,mts,cts}',
+    workspaceFolder: 'test/test.code-workspace',
 });
