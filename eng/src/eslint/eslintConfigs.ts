5ad605a2dbc0e281ce6178ffc44413e1b0bbc463
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-09 10:57:11 -0400

    Less strict stylistic rules

--- eng/src/eslint/eslintConfigs.ts
+++ eng/src/eslint/eslintConfigs.ts
@@ -36,6 +36,8 @@ export const azExtUniversalRules: EslintConfig = {
 export const azExtStylisticRules: EslintConfig = {
     rules: {
         /* eslint-disable @typescript-eslint/naming-convention */
+        '@typescript-eslint/array-type': 'off',
+        '@typescript-eslint/class-literal-property-style': 'off',
         '@typescript-eslint/consistent-indexed-object-style': 'off',
         '@typescript-eslint/consistent-type-assertions': 'off',
         '@typescript-eslint/consistent-type-definitions': 'off',
