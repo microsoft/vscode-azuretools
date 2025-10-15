24f82cc13e37d8cd425f23af93782894dab58bc9
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-15 16:54:11 -0400

    auth: Now with less resolving (#2100)

--- auth/.eslintrc.js
+++ auth/.eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-    "extends": "@microsoft/eslint-config-azuretools"
+    "extends": "@microsoft/eslint-config-azuretools",
+    "rules": {
+        "@typescript-eslint/no-restricted-imports": ["error", {
+            "patterns": [
+                {
+                    "group": ["@azure/*", "!@azure/ms-rest-azure-env"],
+                    "message": "lazy",
+                    "allowTypeImports": true
+                }
+            ]
+        }]
+    }
 };
