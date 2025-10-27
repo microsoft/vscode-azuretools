7bb9fd234fdfb1666263e24c9e5be4740c50520d
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-27 10:38:33 -0400

    Impl and test display name screening

--- auth/src/utils/screen.ts
+++ auth/src/utils/screen.ts
@@ -6,15 +6,55 @@
 import { AzureAccount } from '../contracts/AzureAccount';
 import { AzureTenant } from '../contracts/AzureTenant';
 
-export function screen(accountOrTenant: AzureAccount | AzureTenant): string {
-    if ('label' in accountOrTenant && typeof accountOrTenant.label === 'string') {
-        const label = accountOrTenant.label;
-        return label;
+// These regexs are not perfect, but should work for common cases
+// If they don't work, we'll just return the account ID, which does not need to be screened
+const accountLabelRegex = /^(?<email>[^@]+)@(?<domain>[\w]+(\.[\w]+)+)$/i;
+const domainRegex = /^(?<domain>[^.]+)(?<safeTld>\.com(\..*)?|\.net|\.org|\.co\..*|\.gov)$/i;
 
-    } else if ('displayName' in accountOrTenant && typeof accountOrTenant.displayName === 'string') {
-        const displayName = accountOrTenant.displayName;
-        return displayName;
-    } else {
-        return 'unknown';
+/**
+ * Screens the label or display name of an Azure account or tenant so that it can be logged without exposing PII.
+ * @param accountOrTenant The account or tenant to screen the label / display name of
+ * @returns The screened label / display name
+ */
+export function screen(accountOrTenant: AzureAccount | AzureTenant): string {
+    if ('label' in accountOrTenant && !!accountOrTenant.label) {
+        const match = accountLabelRegex.exec(accountOrTenant.label);
+        if (match?.groups?.email && match?.groups?.domain) {
+            let screenedEmail = match.groups.email;
+            if (screenedEmail.length <= 2) {
+                screenedEmail = '***';
+            } else {
+                screenedEmail = `${screenedEmail.at(0)}***${screenedEmail.at(-1)}`;
+            }
+
+            let screenedDomain = match.groups.domain;
+            const domainMatch = domainRegex.exec(match.groups.domain);
+            if (domainMatch?.groups?.domain && domainMatch?.groups?.safeTld) {
+                if (domainMatch.groups.domain.length <= 2) {
+                    screenedDomain = `***${domainMatch.groups.safeTld}`;
+                } else {
+                    screenedDomain = `${domainMatch.groups.domain.at(0)}***${domainMatch.groups.safeTld}`;
+                }
+            } else if (screenedDomain.length <= 2) {
+                screenedDomain = '***';
+            } else {
+                screenedDomain = `${screenedDomain.at(0)}***${screenedDomain.at(-1)}`;
+            }
+
+            return `${screenedEmail}@${screenedDomain}`;
+        }
+
+        // If we can't match it with our simple regex, just return the account ID instead
+        return accountOrTenant.id;
+    } else if ('displayName' in accountOrTenant && !!accountOrTenant.displayName) {
+        if (accountOrTenant.displayName.length <= 2) {
+            // For too-short names, just return the ID
+            return accountOrTenant.tenantId;
+        } else {
+            // Return the first character, three stars, and the last character
+            return `${accountOrTenant.displayName.at(0)}***${accountOrTenant.displayName.at(-1)}`;
+        }
     }
+
+    return 'unknown';
 }
