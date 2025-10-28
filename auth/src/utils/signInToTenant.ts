a564193e729b5a0a7aac9ddbb870d3c593395668
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-28 09:04:23 -0400

    Allow signInToTenant without specifying account

--- auth/src/utils/signInToTenant.ts
+++ auth/src/utils/signInToTenant.ts
@@ -6,19 +6,20 @@
 import * as vscode from 'vscode';
 import type { AzureAccount } from '../contracts/AzureAccount';
 import type { AzureSubscriptionProvider, TenantIdAndAccount } from '../contracts/AzureSubscriptionProvider';
+import type { AzureTenant } from '../contracts/AzureTenant';
 
 /**
  * Prompts user to select from a list of unauthenticated tenants.
  * Once selected, requests a new session from VS Code specifially for this tenant.
  */
-export async function signInToTenant(subscriptionProvider: AzureSubscriptionProvider, account: AzureAccount): Promise<void> {
+export async function signInToTenant(subscriptionProvider: AzureSubscriptionProvider, account?: AzureAccount): Promise<void> {
     const tenant = await pickTenant(subscriptionProvider, account);
     if (tenant) {
         await subscriptionProvider.signIn(tenant);
     }
 }
 
-async function pickTenant(subscriptionProvider: AzureSubscriptionProvider, account: AzureAccount): Promise<TenantIdAndAccount | undefined> {
+async function pickTenant(subscriptionProvider: AzureSubscriptionProvider, account?: AzureAccount): Promise<TenantIdAndAccount | undefined> {
     const pick = await vscode.window.showQuickPick(getPicks(subscriptionProvider, account), {
         placeHolder: vscode.l10n.t('Select a Tenant (Directory) to Sign In To'),
         matchOnDescription: true, // allow searching by tenantId
@@ -31,8 +32,14 @@ interface TenantQuickPickItem extends vscode.QuickPickItem {
     tenant: TenantIdAndAccount;
 }
 
-async function getPicks(subscriptionProvider: AzureSubscriptionProvider, account: AzureAccount): Promise<TenantQuickPickItem[]> {
-    const unauthenticatedTenants = await subscriptionProvider.getUnauthenticatedTenants(account);
+async function getPicks(subscriptionProvider: AzureSubscriptionProvider, account?: AzureAccount): Promise<TenantQuickPickItem[]> {
+    const unauthenticatedTenants: AzureTenant[] = [];
+    const accounts = account ? [account] : await subscriptionProvider.getAccounts();
+
+    for (const account of accounts) {
+        unauthenticatedTenants.push(...await subscriptionProvider.getUnauthenticatedTenantsForAccount(account));
+    }
+
     const duplicateTenants: Set<string | undefined> = new Set(
         unauthenticatedTenants
             .filter((tenant, index, self) => index !== self.findIndex(t => t.tenantId === tenant.tenantId))
