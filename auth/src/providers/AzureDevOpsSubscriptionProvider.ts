25a5d4e382f2f1d5882b2a2b8c7dd4015e1ee0b1
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-30 15:57:35 -0400

    Use `AzurePipelinesCredential` instead of our custom thing

--- auth/src/providers/AzureDevOpsSubscriptionProvider.ts
+++ auth/src/providers/AzureDevOpsSubscriptionProvider.ts
@@ -3,16 +3,18 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import type { SubscriptionClient } from '@azure/arm-resources-subscriptions';
+import type { SubscriptionClient } from '@azure/arm-resources-subscriptions'; // Keep this as `import type` to avoid actually loading the package before necessary
 import type { TokenCredential } from '@azure/core-auth'; // Keep this as `import type` to avoid actually loading the package (at all, this one is dev-only)
-import type { PipelineRequest } from '@azure/core-rest-pipeline';
+import * as azureEnv from '@azure/ms-rest-azure-env'; // This package is so small that it's not worth lazy loading
+import * as crypto from 'crypto';
+import type * as vscode from 'vscode';
 import type { AzureAccount } from '../contracts/AzureAccount';
 import type { AzureAuthentication } from '../contracts/AzureAuthentication';
-import type { AzureSubscription } from '../contracts/AzureSubscription';
-import type { AzureSubscriptionProvider, GetOptions, GetSubscriptionsOptions, SignInOptions, TenantIdAndAccount } from '../contracts/AzureSubscriptionProvider';
+import type { GetOptions, SignInOptions, TenantIdAndAccount } from '../contracts/AzureSubscriptionProvider';
 import type { AzureTenant } from '../contracts/AzureTenant';
-import { getConfiguredAzureEnv } from '../utils/configuredAzureEnv';
+import { isAuthenticationWwwAuthenticateRequest } from '../utils/isAuthenticationWwwAuthenticateRequest';
 import { NotSignedInError } from '../utils/NotSignedInError';
+import { AzureSubscriptionProviderBase } from './AzureSubscriptionProviderBase';
 
 export interface AzureDevOpsSubscriptionProviderInitializer {
     /**
@@ -22,9 +24,9 @@ export interface AzureDevOpsSubscriptionProviderInitializer {
     */
     serviceConnectionId: string,
     /**
-    * The `Tenant ID` field of the service connection properties
-    */
-    domain: string,
+     * The `Tenant ID` field of the service connection properties
+     */
+    tenantId: string,
     /**
     * The `Service Principal Id` field of the service connection properties
     */
@@ -39,68 +41,46 @@ export function createAzureDevOpsSubscriptionProviderFactory(initializer: AzureD
     }
 }
 
+let armSubs: typeof import('@azure/arm-resources-subscriptions') | undefined;
+
 /**
  * AzureSubscriptionProvider implemented to authenticate via federated DevOps service connection, using workflow identity federation
  * To learn how to configure your DevOps environment to use this provider, refer to the README.md
  * NOTE: This provider is only available when running in an Azure DevOps pipeline
  * Reference: https://learn.microsoft.com/en-us/entra/workload-id/workload-identity-federation
  */
-export class AzureDevOpsSubscriptionProvider implements AzureSubscriptionProvider {
-    private _tokenCredential: TokenCredential & { tenantId?: string } | undefined;
-    /**
-    * The resource ID of the Azure DevOps federated service connection,
-    *   which can be found on the `resourceId` field of the URL at the address bar
-    *   when viewing the service connection in the Azure DevOps portal
-    */
+export class AzureDevOpsSubscriptionProvider extends AzureSubscriptionProviderBase {
+    private _tokenCredential: TokenCredential | undefined;
     private _SERVICE_CONNECTION_ID: string;
-    /**
-    * The `Tenant ID` field of the service connection properties
-    */
-    private _DOMAIN: string;
-    /**
-    * The `Service Principal Id` field of the service connection properties
-    */
+    private _TENANT_ID: string;
     private _CLIENT_ID: string;
 
-    public constructor({ serviceConnectionId, domain, clientId }: AzureDevOpsSubscriptionProviderInitializer) {
-        if (!serviceConnectionId || !domain || !clientId) {
+    public constructor({ serviceConnectionId, tenantId, clientId }: AzureDevOpsSubscriptionProviderInitializer, logger?: vscode.LogOutputChannel) {
+        super(logger);
+
+        if (!serviceConnectionId || !tenantId || !clientId) {
             throw new Error(`Missing initializer values to identify Azure DevOps federated service connection\n
                 Values provided:\n
                 serviceConnectionId: ${serviceConnectionId ? "✅" : "❌"}\n
-                domain: ${domain ? "✅" : "❌"}\n
+                tenantId: ${tenantId ? "✅" : "❌"}\n
                 clientId: ${clientId ? "✅" : "❌"}\n
             `);
         }
 
         this._SERVICE_CONNECTION_ID = serviceConnectionId;
-        this._DOMAIN = domain;
+        this._TENANT_ID = tenantId;
         this._CLIENT_ID = clientId;
     }
 
     /**
-     * @inheritdoc
+     * For {@link AzureSubscriptionProviderBase}, this event will never fire
      */
-    public readonly onRefreshSuggested = () => { return { dispose: () => { /* empty */ } }; };
+    public override onRefreshSuggested = () => { return { dispose: () => { /* empty */ } }; };
 
     /**
-     * @inheritdoc
+     * For {@link AzureSubscriptionProviderBase}, this returns a single account with a fixed ID and label
      */
-    public async getAvailableSubscriptions(_options?: GetOptions): Promise<AzureSubscription[]> {
-        const allSubscriptions: AzureSubscription[] = [];
-
-        for (const account of await this.getAccounts()) {
-            for (const tenant of await this.getTenantsForAccount(account)) {
-                allSubscriptions.push(...await this.getSubscriptionsForTenant(tenant));
-            }
-        }
-
-        return allSubscriptions;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public async getAccounts(_options?: GetOptions): Promise<AzureAccount[]> {
+    public override async getAccounts(_options?: GetOptions): Promise<AzureAccount[]> {
         return [
             {
                 id: 'test-account-id',
@@ -110,19 +90,19 @@ export class AzureDevOpsSubscriptionProvider implements AzureSubscriptionProvide
     }
 
     /**
-     * @inheritdoc
+     * For {@link AzureSubscriptionProviderBase}, this returns an empty array
      */
-    public async getUnauthenticatedTenantsForAccount(_account: AzureAccount, _options?: GetOptions): Promise<AzureTenant[]> {
+    public override async getUnauthenticatedTenantsForAccount(_account: AzureAccount, _options?: GetOptions): Promise<AzureTenant[]> {
         // For DevOps federated service connection, there is only one tenant associated with the service principal, and we will be authenticated
         return [];
     }
 
     /**
-     * @inheritdoc
+     * For {@link AzureSubscriptionProviderBase}, this returns a single tenant associated with the service principal
      */
-    public async getTenantsForAccount(account: AzureAccount, _options?: GetOptions): Promise<AzureTenant[]> {
+    public override async getTenantsForAccount(account: AzureAccount, _options?: GetOptions): Promise<AzureTenant[]> {
         return [{
-            tenantId: 'test-tenant-id',
+            tenantId: this._TENANT_ID,
             account: account,
         }]
     }
@@ -130,141 +110,70 @@ export class AzureDevOpsSubscriptionProvider implements AzureSubscriptionProvide
     /**
      * @inheritdoc
      */
-    public async signIn(_tenant?: TenantIdAndAccount, _options?: SignInOptions): Promise<boolean> {
-        this._tokenCredential = await getTokenCredential(this._SERVICE_CONNECTION_ID, this._DOMAIN, this._CLIENT_ID);
+    public override async signIn(_tenant?: TenantIdAndAccount, _options?: SignInOptions): Promise<boolean> {
+        this._tokenCredential ??= await getTokenCredential(this._SERVICE_CONNECTION_ID, this._TENANT_ID, this._CLIENT_ID);
         return !!this._tokenCredential;
     }
 
     /**
      * @inheritdoc
      */
-    public async getSubscriptionsForTenant(tenant: TenantIdAndAccount, _options?: GetSubscriptionsOptions): Promise<AzureSubscription[]> {
-        const { client, credential, authentication } = await this.getSubscriptionClient();
-        const environment = getConfiguredAzureEnv();
-
-        const subscriptions: AzureSubscription[] = [];
-
-        for await (const subscription of client.subscriptions.list()) {
-            subscriptions.push({
-                authentication,
-                environment: environment,
-                credential: credential,
-                isCustomCloud: environment.isCustomCloud,
-                /* eslint-disable @typescript-eslint/no-non-null-assertion */
-                name: subscription.displayName!,
-                subscriptionId: subscription.subscriptionId!,
-                /* eslint-enable @typescript-eslint/no-non-null-assertion */
-                tenantId: subscription.tenantId || tenant.tenantId,
-                account: tenant.account,
-            });
-        }
-
-        return subscriptions.sort((a, b) => a.name.localeCompare(b.name));
-    }
-
-    /**
-     * Gets a fully-configured subscription client for a given tenant ID
-     *
-     * @param scopes Optional scopes to request in the authentication token
-     *
-     * @returns A client, the credential used by the client, and the authentication function
-     */
-    private async getSubscriptionClient(scopes?: string[]): Promise<{ client: SubscriptionClient, credential: TokenCredential, authentication: AzureAuthentication }> {
-        const armSubs = await import('@azure/arm-resources-subscriptions');
+    protected override async getSubscriptionClient(tenant: TenantIdAndAccount): Promise<{ client: SubscriptionClient, credential: TokenCredential, authentication: AzureAuthentication }> {
         if (!this._tokenCredential) {
             throw new NotSignedInError();
         }
 
-        const accessToken = (await this._tokenCredential?.getToken("https://management.azure.com/.default"))?.token || '';
-        const getSession = () => {
+        const getSessionWithScopes = async (scopes: string[] | vscode.AuthenticationWwwAuthenticateRequest) => {
+            if (isAuthenticationWwwAuthenticateRequest(scopes)) {
+                throw new Error('Getting session with challenge is not supported in AzureDevOpsSubscriptionProvider.');
+            }
+
+            const token = await this._tokenCredential?.getToken(scopes);
+
+            if (!token) {
+                throw new NotSignedInError();
+            }
+
             return {
-                accessToken,
-                id: this._tokenCredential?.tenantId || '',
-                account: {
-                    id: this._tokenCredential?.tenantId || '',
-                    label: this._tokenCredential?.tenantId || '',
-                },
-                tenantId: this._tokenCredential?.tenantId || '',
+                accessToken: token.token,
+                id: crypto.randomUUID(),
+                account: tenant.account,
                 scopes: scopes || [],
-            };
+            } satisfies vscode.AuthenticationSession;
         };
+
+        armSubs ??= await import('@azure/arm-resources-subscriptions');
+
         return {
-            client: new armSubs.SubscriptionClient(this._tokenCredential,),
+            client: new armSubs.SubscriptionClient(this._tokenCredential),
             credential: this._tokenCredential,
             authentication: {
-                getSession,
-                getSessionWithScopes: getSession,
+                getSession: () => {
+                    return getSessionWithScopes([azureEnv.Environment.AzureCloud.managementEndpointUrl + '/.default']);
+                },
+                getSessionWithScopes: getSessionWithScopes,
             }
         }
     }
 }
 
-/*
+/**
 * @param serviceConnectionId The resource ID of the Azure DevOps federated service connection,
 *   which can be found on the `resourceId` field of the URL at the address bar when viewing the service connection in the Azure DevOps portal
-* @param domain The `Tenant ID` field of the service connection properties
+* @param tenantId The `Tenant ID` field of the service connection properties
 * @param clientId The `Service Principal Id` field of the service connection properties
 */
-async function getTokenCredential(serviceConnectionId: string, domain: string, clientId: string): Promise<TokenCredential> {
+async function getTokenCredential(serviceConnectionId: string, tenantId: string, clientId: string): Promise<TokenCredential> {
     if (!process.env.AGENT_BUILDDIRECTORY) {
         // Assume that AGENT_BUILDDIRECTORY is set if running in an Azure DevOps pipeline.
         // So when not running in an Azure DevOps pipeline, throw an error since we cannot use the DevOps federated service connection credential.
-        throw new Error(`Cannot create DevOps federated service connection credential outside of an Azure DevOps pipeline.`);
+        throw new Error('Cannot create DevOps federated service connection credential outside of an Azure DevOps pipeline.');
+    } else if (!process.env.SYSTEM_ACCESSTOKEN) {
+        throw new Error('Cannot create DevOps federated service connection credential because the SYSTEM_ACCESSTOKEN environment variable is not set.');
     } else {
-        console.log(`Creating DevOps federated service connection credential for service connection..`);
+        console.log('Creating DevOps federated service connection credential for service connection...');
 
-        // Pre-defined DevOps variable reference: https://learn.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops
-        const systemAccessToken = process.env.SYSTEM_ACCESSTOKEN;
-        const teamFoundationCollectionUri = process.env.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI;
-        const teamProjectId = process.env.SYSTEM_TEAMPROJECTID;
-        const planId = process.env.SYSTEM_PLANID;
-        const jobId = process.env.SYSTEM_JOBID;
-
-        if (!systemAccessToken || !teamFoundationCollectionUri || !teamProjectId || !planId || !jobId) {
-            throw new Error(`Azure DevOps environment variables are not set.\n
-            process.env.SYSTEM_ACCESSTOKEN: ${process.env.SYSTEM_ACCESSTOKEN ? "✅" : "❌"}\n
-            process.env.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI: ${process.env.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI ? "✅" : "❌"}\n
-            process.env.SYSTEM_TEAMPROJECTID: ${process.env.SYSTEM_TEAMPROJECTID ? "✅" : "❌"}\n
-            process.env.SYSTEM_PLANID: ${process.env.SYSTEM_PLANID ? "✅" : "❌"}\n
-            process.env.SYSTEM_JOBID: ${process.env.SYSTEM_JOBID ? "✅" : "❌"}\n
-            REMEMBER: process.env.SYSTEM_ACCESSTOKEN must be explicitly mapped!\n
-            https://learn.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml#systemaccesstoken
-        `);
-        }
-
-        const oidcRequestUrl = `${teamFoundationCollectionUri}${teamProjectId}/_apis/distributedtask/hubs/build/plans/${planId}/jobs/${jobId}/oidctoken?api-version=7.1-preview.1&serviceConnectionId=${serviceConnectionId}`;
-
-        const { ClientAssertionCredential } = await import("@azure/identity");
-        return new ClientAssertionCredential(domain, clientId, async () => await requestOidcToken(oidcRequestUrl, systemAccessToken));
+        const { AzurePipelinesCredential } = await import('@azure/identity');
+        return new AzurePipelinesCredential(tenantId, clientId, serviceConnectionId, process.env.SYSTEM_ACCESSTOKEN);
     }
-}
-
-/**
- * API reference: https://learn.microsoft.com/en-us/rest/api/azure/devops/distributedtask/oidctoken/create
- */
-async function requestOidcToken(oidcRequestUrl: string, systemAccessToken: string): Promise<string> {
-    const { ServiceClient } = await import('@azure/core-client');
-    const { createHttpHeaders, createPipelineRequest } = await import('@azure/core-rest-pipeline');
-    const genericClient = new ServiceClient();
-    const request: PipelineRequest = createPipelineRequest({
-        url: oidcRequestUrl,
-        method: "POST",
-        headers: createHttpHeaders({
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${systemAccessToken}`
-        })
-    });
-
-    const response = await genericClient.sendRequest(request);
-    const body: string = response.bodyAsText?.toString() || "";
-    if (response.status !== 200) {
-        throw new Error(`Failed to get OIDC token:\n
-            Response status: ${response.status}\n
-            Response body: ${body}\n
-            Response headers: ${JSON.stringify(response.headers.toJSON())}
-        `);
-    } else {
-        console.log(`Successfully got OIDC token with status ${response.status}`);
-    }
-    return (JSON.parse(body) as { oidcToken: string }).oidcToken;
 }
