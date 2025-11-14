dcac1b6501a0ba63eb89e0ff659db13aae1593fb
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-11-14 13:27:32 -0500

    Use the stricter lint rules

--- auth/eslint.config.mjs
+++ auth/eslint.config.mjs
@@ -3,10 +3,10 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { azExtEslintRecommended, lazyImportAzurePackages } from '@microsoft/vscode-azext-eng/eslint'; // Other configurations exist
+import { azExtEslintStrictTypeChecked, lazyImportAzurePackages } from '@microsoft/vscode-azext-eng/eslint'; // Other configurations exist
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-    azExtEslintRecommended,
+    azExtEslintStrictTypeChecked,
     lazyImportAzurePackages,
 ]);
