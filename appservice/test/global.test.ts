369a689b997194b3bb94ce67427b45693c1d8b89
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-12-08 17:02:47 -0500

    Tests

--- appservice/test/global.test.ts
+++ appservice/test/global.test.ts
@@ -3,24 +3,30 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as fse from 'fs-extra';
-import * as path from 'path';
-import * as vscode from 'vscode';
+// TODO: when available, use `testGlobalSetup` from @microsoft/vscode-azext-utils
 
-export const testWorkspaceRoot: string = path.resolve(__dirname, '..', '..', '..', 'testWorkspace');
+import { createAzExtOutputChannel, registerUIExtensionVariables } from '@microsoft/vscode-azext-utils';
+import * as path from 'path';
+import { registerAppServiceExtensionVariables } from '../src';
+
+export const testWorkspaceRoot: string = path.resolve(__dirname, '..', 'testWorkspace');
 
 // Runs before all tests
-suiteSetup(async () => {
-    const id: string = 'ms-azuretools.azureappservice';
-    const extension: vscode.Extension<unknown> | undefined = vscode.extensions.getExtension(id);
-    if (!extension) {
-        throw new Error(`Failed to activate extension with id "${id}".`);
-    } else {
-        await extension.activate();
-    }
-
-    const folders = vscode.workspace.workspaceFolders || [];
-    for (const folder of folders) {
-        await fse.ensureDir(folder.uri.fsPath);
-    }
+suiteSetup(() => {
+    const extVars = {
+        context: {
+            extension: {
+                packageJSON: {
+                    name: 'azureextensionui',
+                    publisher: 'ms-azuretools',
+                    version: '0.0.1',
+                    aiKey: '00000000-0000-0000-0000-000000000000'
+                },
+            },
+            subscriptions: [],
+        } as any,
+        outputChannel: createAzExtOutputChannel('Extension Test Output', 'azureextensionui')
+    };
+    registerUIExtensionVariables(extVars);
+    registerAppServiceExtensionVariables(extVars as any);
 });
