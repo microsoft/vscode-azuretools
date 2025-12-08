53a324647be4c09d7813ca3cb9b0d8d385cf4bdd
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-12-08 16:45:20 -0500

    appsettings: Move to new eng package

---
+++ appsettings/eslint.config.mjs
@@ -0,0 +1,6 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export { azExtEslintRecommendedTypeChecked as default } from '@microsoft/vscode-azext-eng/eslint'; // Other configurations exist
