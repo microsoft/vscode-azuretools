7f5af0d66ef1dbb9dca6d3a7ab9a8f0c84bd7013
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-12-08 15:46:21 -0500

    todo

--- utils/.vscode-test.mjs
+++ utils/.vscode-test.mjs
@@ -3,12 +3,4 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { azExtTestConfig } from '@microsoft/vscode-azext-eng/vscode-test'; // Other configurations exist
-
-export default { // TODO: Remove this when using 1.0.0-alpha.5 of the eng pkg
-    ...azExtTestConfig,
-    env: {
-        ...azExtTestConfig.env,
-        DEBUGTELEMETRY: '1',
-    },
-};
+export { azExtTestConfig as default } from '@microsoft/vscode-azext-eng/vscode-test'; // Other configurations exist
