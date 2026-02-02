1ce6e939a39af6e323a291027e781208b8f15ff4
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2026-02-02 13:31:10 -0800

    Upgrade appservice sdk, update domain name label scope enum and selection step logic

--- appservice/src/createAppService/SiteDomainNameLabelScopeStep.ts
+++ appservice/src/createAppService/SiteDomainNameLabelScopeStep.ts
@@ -11,20 +11,21 @@ export enum DomainNameLabelScope {
     ResourceGroup = 'ResourceGroupReuse',
     Subscription = 'SubscriptionReuse',
     Tenant = 'TenantReuse',
-    Global = 'NoReuse',
+    NoReuse = 'NoReuse',
 }
 
 export class SiteDomainNameLabelScopeStep<T extends IAppServiceWizardContext> extends AzureWizardPromptStep<T> {
     public async prompt(context: T): Promise<void> {
-        const picks: IAzureQuickPickItem<DomainNameLabelScope | undefined>[] = [
+        const learnMore = 'learnMore';
+        const picks: IAzureQuickPickItem<DomainNameLabelScope | typeof learnMore | undefined>[] = [
             // Matching the portal which doesn't yet offer ResourceGroup and Subscription level domain scope
             { label: vscode.l10n.t('Secure unique default hostname'), description: vscode.l10n.t('Tenant Scope'), data: DomainNameLabelScope.Tenant },
-            { label: vscode.l10n.t('Global default hostname'), description: vscode.l10n.t('Global'), data: DomainNameLabelScope.Global },
-            { label: vscode.l10n.t('$(link-external) Learn more about unique default hostname'), data: undefined },
+            { label: vscode.l10n.t('Global default hostname'), description: vscode.l10n.t('Global'), data: undefined },
+            { label: vscode.l10n.t('$(link-external) Learn more about unique default hostname'), data: learnMore },
         ];
         const learnMoreUrl: string = 'https://aka.ms/AAu7lhs';
 
-        let result: DomainNameLabelScope | undefined;
+        let result: DomainNameLabelScope | typeof learnMore | undefined;
         do {
             result = (await context.ui.showQuickPick(picks, {
                 placeHolder: vscode.l10n.t('Select default hostname format'),
@@ -32,13 +33,13 @@ export class SiteDomainNameLabelScopeStep<T extends IAppServiceWizardContext> ex
                 learnMoreLink: learnMoreUrl,
             })).data;
 
-            if (!result) {
+            if (result === learnMore) {
                 await openUrl(learnMoreUrl);
             }
-        } while (!result);
+        } while (result === learnMore);
 
-        context.telemetry.properties.siteDomainNameLabelScope = result;
         context.newSiteDomainNameLabelScope = result;
+        context.telemetry.properties.siteDomainNameLabelScope = context.newSiteDomainNameLabelScope;
     }
 
     public shouldPrompt(context: T): boolean {
