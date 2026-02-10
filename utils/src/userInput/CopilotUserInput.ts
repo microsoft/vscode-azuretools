a6f34c8039c07ff7f42e69fe55c54edd7b553b9e
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2026-02-10 15:53:17 -0500

    Lint issues

--- utils/src/userInput/CopilotUserInput.ts
+++ utils/src/userInput/CopilotUserInput.ts
@@ -2,6 +2,7 @@
 *  Copyright (c) Microsoft Corporation. All rights reserved.
 *  Licensed under the MIT License. See License.md in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
+
 import type * as vscodeTypes from 'vscode';
 import { workspace } from 'vscode';
 import * as types from '../../index';
