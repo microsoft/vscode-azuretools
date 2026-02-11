83aa92639585e5ff06be88798d0acd6c3fd060c2
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2026-02-11 09:49:49 -0500

    Deprecate `registerGitHubExtensionVariables`

--- github/src/extensionVariables.ts
+++ github/src/extensionVariables.ts
@@ -7,6 +7,7 @@ import { registerUIExtensionVariables, UIExtensionVariables } from '@microsoft/v
 
 /**
  * Call this to register common variables used throughout the github package.
+ * @deprecated This method is no longer necessary and will be removed in a future release. You can safely remove any calls to this method.
  */
 export function registerGitHubExtensionVariables(extVars: UIExtensionVariables): void {
     registerUIExtensionVariables(extVars);
